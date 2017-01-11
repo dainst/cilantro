aa2d17fa896d38eb31b844799eea3889623827d1
Philipp Franck <philipp.franck@dainst.de>
2017-01-11 11:28:11 +0100

    removed dependencies from from

--- bower_components/idai-components/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Author: Daniel M. de Oliveira
-
-rm dest/*js
-grunt html2js
-sed -e 's/\.\.\/src\/partials/partials/g' -e 's/templates-main/idai\.components/g' dest/templates.js > dest/templates.seded.js
-grunt uglify
