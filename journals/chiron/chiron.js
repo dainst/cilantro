5b9868843162139c983c407c84b49f325c6c5e56
Philipp Franck <philipp.franck@dainst.de>
2017-01-13 12:54:56 +0100

    test commit to test redmine connection

--- journals/chiron/chiron.js
+++ journals/chiron/chiron.js
@@ -210,6 +210,7 @@ angular
 			chiron.createdThumbnails += 1;
 			return;
 		}
+
 		
 		var container = angular.element(document.querySelector('#thumbnail-container-' + containerId));
 		img = container.find('img');
