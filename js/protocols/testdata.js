badaab4ba9672dc7ed4211b403dc941b4dc73b13
Philipp Franck <philipp.franck@dainst.de>
2017-04-28 11:53:19 +0200

    repaired testdata protocol

--- js/protocols/testdata.js
+++ js/protocols/testdata.js
@@ -2,45 +2,47 @@
 
 angular
 .module('module.protocols.testdata', [])
-.factory("testdata", ['$log', 'editables', 'protocolregistry', function($log, editables, protocolregistry) {
+.factory("testdata", ['protocolregistry', 'journal',
+	function(protocolregistry, journal) {
 
 
 	var journalCtrl = new protocolregistry.Protocol('testdata');
 
 	journalCtrl.description = "Create some Testdata 3.0";
 
+	journalCtrl.startView = 'articles';
+
 
 	/* the journal's settings */
 	journalCtrl.columns = ['title'];
 
 	journalCtrl.init = function() {
-		journalCtrl.main.articles.push(new journalCtrl.main.Article({
+		journal.articles.push(new journal.Article({
 			title: "A lady centaur",
 			author: {firstname:'1peter', lastname: '1parker'},
 			pages: 1
 		}));
-		journalCtrl.main.articles.push(new journalCtrl.main.Article({
+		journal.articles.push(new journal.Article({
 			title: "Something else",
 			author: {firstname:'2peter', lastname: '2parker'},
 			pages: 1
 		}));
-		journalCtrl.main.articles.push(new journalCtrl.main.Article({
+		journal.articles.push(new journal.Article({
 			title: "Geh, Schwurbel!",
 			author: {firstname:'3peter', lastname: '3parker'},
 			pages: 1
 		}));
-		journalCtrl.main.articles.push(new journalCtrl.main.Article({
+		journal.articles.push(new journal.Article({
 			title: "DER ARTIKEL",
 			author: {firstname:'4Piller', lastname: '4Mann'},
 			pages: 1
 		}));
-		journalCtrl.main.articles.push(new journalCtrl.main.Article({
+		journal.articles.push(new journal.Article({
 			title: "Alter, wie das Nervt",
 			author: {firstname:'5Piller', lastname: '5Mann'},
 			pages: 1
 		}));
-		console.log(journalCtrl.main.articles);
-		journalCtrl.main.steps.change('articles');
+
 	};
 
 
