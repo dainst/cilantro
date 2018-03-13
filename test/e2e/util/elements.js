1949b231817e6a7da98679045b3cdd3234374cd1
ngrippa <nemo.grippa@dainst.de>
2018-03-13 16:38:18 +0100

    wrote test for import abort

--- test/e2e/util/elements.js
+++ test/e2e/util/elements.js
@@ -4,7 +4,10 @@ module.exports = {
         passwordInput: element(by.css('h1 input[type="password"]')),
         mainMessage: element(by.css('#info-container .alert.main-message')),
         protocolSelect: element(by.model("protocols.current")),
-        startBtn: element(by.css('.row.toprow .btn-primary'))
+        startBtn: element(by.css('.row.toprow .btn-primary')),
+        restartBtn: element(by.css('#step-restart')),
+        confirmRestartBtn: element(by.css('.btn-danger'))
+
     },
     articles: {},
     finish: {},
