910fbbec84759f51996114371f544b3aa1562586
Sebastian Cuy <sebastian.cuy@dainst.de>
2018-04-10 15:03:15 +0200

    add redis result backend

--- service/celery_client.py
+++ service/celery_client.py
@@ -5,8 +5,9 @@ from celery import Celery
 broker_host = os.environ['BROKER_HOST']
 broker_user = os.environ['BROKER_USER']
 broker_password = os.environ['BROKER_PASSWORD']
+db_host = os.environ['DB_HOST']
 
 broker_config = 'amqp://' + broker_user + ':' + broker_password + '@' + broker_host + '/'
-result_config = 'rpc://'
+result_config = 'redis://' + db_host
 
 celery = Celery('cilantro', broker=broker_config, backend=result_config)
