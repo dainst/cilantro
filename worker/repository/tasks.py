ab5c208c5587b628ebcd0c429038d3c15f97cfb2
Sebastian Cuy <sebastian.cuy@dainst.de>
2018-04-13 16:19:02 +0200

    structure worker modules thematically

--- worker/repository/tasks.py
+++ worker/repository/tasks.py
@@ -7,6 +7,19 @@ repository_dir = os.environ['REPOSITORY_DIR']
 working_dir = os.environ['WORKING_DIR']
 
 
+@celery.task(name="tasks.retrieve")
+def retrieve(object_id):
+    if not os.path.exists(repository_dir):
+        os.makedirs(repository_dir)
+    if not os.path.exists(working_dir):
+        os.makedirs(working_dir)
+    source = os.path.join(repository_dir, object_id, 'upload')
+    target = os.path.join(working_dir, object_id, 'retrieve')
+    if os.path.exists(target):
+        shutil.rmtree(target)
+    shutil.copytree(source, target)
+
+
 @celery.task(name="tasks.publish")
 def publish(object_id, prev_task):
     source = os.path.join(working_dir, object_id, prev_task)
--- worker/tasks.py
+++ worker/tasks.py
@@ -2,8 +2,6 @@ from utils.celery_client import celery
 
 
 celery.autodiscover_tasks([
-    'worker.convert',
-    'worker.match',
-    'worker.publish',
-    'worker.retrieve'
+    'worker.repository',
+    'worker.utils'
 ])
