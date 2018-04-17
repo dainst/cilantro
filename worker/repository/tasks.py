c67600d68a64f8ae1e9c1129350f91f665d3eb09
Sebastian Cuy <sebastian.cuy@dainst.de>
2018-04-17 17:06:05 +0200

    rename repository tasks

--- worker/repository/tasks.py
+++ worker/repository/tasks.py
@@ -8,21 +8,21 @@ working_dir = os.environ['WORKING_DIR']
 staging_dir = os.environ['STAGING_DIR']
 
 
-@celery.task(name="tasks.retrieve", bind=True)
-def retrieve(self, object_id, job_id):
+@celery.task(name="tasks.retrieve_from_staging", bind=True)
+def retrieve_from_staging(self, object_id, job_id):
     if not os.path.exists(staging_dir):
         os.makedirs(staging_dir)
     if not os.path.exists(working_dir):
         os.makedirs(working_dir)
     source = os.path.join(staging_dir, object_id)
-    target = os.path.join(working_dir, job_id, object_id, 'retrieve')
+    target = os.path.join(working_dir, job_id, object_id, "retrieve_from_staging")
     if os.path.exists(target):
         shutil.rmtree(target)
     shutil.copytree(source, target)
 
 
-@celery.task(name="tasks.publish")
-def publish(object_id, job_id, prev_task):
+@celery.task(name="tasks.publish_to_repository")
+def publish_to_repository(object_id, job_id, prev_task):
     source = os.path.join(working_dir, job_id, object_id, prev_task)
     target = os.path.join(repository_dir, object_id)
     if os.path.exists(target):
