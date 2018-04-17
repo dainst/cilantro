15f55a683f4c8d3760a2140cb90d95bfb25aa856
Sebastian Cuy <sebastian.cuy@dainst.de>
2018-04-17 11:32:02 +0200

    introduce staging area
    closes #8269

--- worker/repository/tasks.py
+++ worker/repository/tasks.py
@@ -5,15 +5,16 @@ from utils.celery_client import celery
 
 repository_dir = os.environ['REPOSITORY_DIR']
 working_dir = os.environ['WORKING_DIR']
+staging_dir = os.environ['STAGING_DIR']
 
 
 @celery.task(name="tasks.retrieve", bind=True)
 def retrieve(self, object_id, job_id):
-    if not os.path.exists(repository_dir):
-        os.makedirs(repository_dir)
+    if not os.path.exists(staging_dir):
+        os.makedirs(staging_dir)
     if not os.path.exists(working_dir):
         os.makedirs(working_dir)
-    source = os.path.join(repository_dir, object_id, 'upload')
+    source = os.path.join(staging_dir, object_id)
     target = os.path.join(working_dir, job_id, object_id, 'retrieve')
     if os.path.exists(target):
         shutil.rmtree(target)
@@ -23,7 +24,7 @@ def retrieve(self, object_id, job_id):
 @celery.task(name="tasks.publish")
 def publish(object_id, job_id, prev_task):
     source = os.path.join(working_dir, job_id, object_id, prev_task)
-    target = os.path.join(repository_dir, object_id, 'publish')
+    target = os.path.join(repository_dir, object_id)
     if os.path.exists(target):
         shutil.rmtree(target)
     shutil.copytree(source, target)
