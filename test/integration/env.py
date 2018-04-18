a21fe8b2e550a104b2b0b09f761621f00c659038
Sebastian Cuy <sebastian.cuy@uni-koeln.de>
2018-04-18 10:14:59 +0200

    extend integration test for job_type 'test'

---
+++ test/integration/env.py
@@ -0,0 +1,10 @@
+import os
+
+os.environ['BROKER_HOST'] = "localhost"
+os.environ['BROKER_USER'] = "user"
+os.environ['BROKER_PASSWORD'] = "password"
+os.environ['DB_HOST'] = "localhost"
+os.environ['CONFIG_DIR'] = "config"
+os.environ['STAGING_DIR'] = "data/staging"
+os.environ['REPOSITORY_DIR'] = "data/repository"
+os.environ['WORKING_DIR'] = "data/workspace"
--- test/integration/test_test.py
+++ test/integration/test_test.py
@@ -1,14 +1,31 @@
-import unittest
 import os
-
+import time
+# noinspection PyUnresolvedReferences
+import test.integration.env
+import unittest
 from flask import json
-
-os.environ['BROKER_HOST'] = "localhost"
-os.environ['BROKER_USER'] = "user"
-os.environ['BROKER_PASSWORD'] = "password"
-os.environ['DB_HOST'] = "localhost"
-os.environ['CONFIG_DIR'] = "config"
 from run_service import app
+from pathlib import Path
+
+
+wait_time = 15000
+retry_time = 100
+
+staging_dir = os.environ['STAGING_DIR']
+working_dir = os.environ['WORKING_DIR']
+repository_dir = os.environ['REPOSITORY_DIR']
+
+
+def _wait_for_file(path):
+    waited = 0
+    file = Path(path)
+    while not file.is_file():
+        if waited > wait_time:
+            return False
+        else:
+            waited += retry_time
+            time.sleep(0.001 * retry_time)
+    return True
 
 
 class JobTypeTestTest(unittest.TestCase):
@@ -17,7 +34,31 @@ class JobTypeTestTest(unittest.TestCase):
         app.testing = True
         self.client = app.test_client()
 
-    def test_empty_db(self):
+    def test_success(self):
         response = self.client.post('/test/foo')
         data = json.loads(response.get_data(as_text=True))
-        self.assertEquals('Accepted', data['status'])
+        self.assertEqual('Accepted', data['status'])
+
+        path = os.path.join(repository_dir, 'foo', 'image1.jpg')
+        self.assertTrue(_wait_for_file(path), "expected %s to be present" % path)
+
+        job_id = data['job_id']
+        self.assertEqual('PENDING', self._get_status(job_id))
+        self.assertTrue(self._wait_for_status(job_id, 'SUCCESS'), "timeout while waiting for status 'SUCCESS'")
+
+    def _wait_for_status(self, job_id, expected_status):
+        waited = 0
+        status = self._get_status(job_id)
+        while status != expected_status:
+            if waited > wait_time:
+                return False
+            else:
+                waited += retry_time
+                time.sleep(0.001 * retry_time)
+            status = self._get_status(job_id)
+        return True
+
+    def _get_status(self, job_id):
+        response = self.client.get('/%s' % job_id)
+        data = json.loads(response.get_data(as_text=True))
+        return data['status']
