7a5e9fde7fad6aa90d2a43cb76c1901e6c93b4bc
Sebastian Cuy <sebastian.cuy@uni-koeln.de>
2018-04-18 15:32:31 +0200

    test tif to jpg conversion for error

--- test/convert/test_tif2jpg.py
+++ test/convert/test_tif2jpg.py
@@ -7,7 +7,14 @@ from convert.converter import convert_tif2jpg
 class Tif2JpgTest(unittest.TestCase):
 
     tif_path = 'test/resources/objects/some_tiffs/test.tif'
+    broken_tif_path = 'test/resources/objects/test_object/image1.tif'
     jpg_path = 'test/resources/objects/some_tiffs/test.jpg'
+
+    def tearDown(self):
+        try:
+            os.remove(self.jpg_path)
+        except FileNotFoundError:
+            pass
 
     def test_success(self):
         convert_tif2jpg(self.tif_path, self.jpg_path)
@@ -15,5 +22,5 @@ class Tif2JpgTest(unittest.TestCase):
         stat = os.stat(self.jpg_path)
         self.assertGreater(stat.st_size, 0)
 
-    def tearDown(self):
-        os.remove(self.jpg_path)
+    def test_error(self):
+        self.assertRaises(OSError, convert_tif2jpg, self.broken_tif_path, self.jpg_path)
