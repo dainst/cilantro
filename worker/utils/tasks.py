324cd806595382ae98e234941f7d0f2bac033f69
Sebastian Cuy <sebastian.cuy@uni-koeln.de>
2018-04-18 14:00:48 +0200

    create folder in match task
    so subtasks do not have to check if it's present

--- worker/utils/tasks.py
+++ worker/utils/tasks.py
@@ -14,6 +14,8 @@ working_dir = os.environ['WORKING_DIR']
 @celery.task(bind=True, name="match")
 def match(self, object_id, job_id, prev_task, run, pattern='*.tif'):
     source = os.path.join(working_dir, job_id, object_id, prev_task)
+    target = os.path.join(working_dir, job_id, object_id, self.name)
+    os.makedirs(target)
     subtasks = []
     for file in glob.iglob(os.path.join(source, pattern)):
         subtasks.append(signature(
@@ -26,11 +28,6 @@ def match(self, object_id, job_id, prev_task, run, pattern='*.tif'):
 def rename(object_id, job_id, prev_task, parent_task, file):
     source = os.path.join(working_dir, job_id, object_id, prev_task)
     target = os.path.join(working_dir, job_id, object_id, parent_task)
-    if not os.path.exists(target):
-        try:
-            os.makedirs(target)
-        except OSError:
-            print("could not create dir, eating exception")
     time.sleep(10)
     new_file = file.replace('.tif', '.jpg').replace(source, target)
     shutil.copyfile(file, new_file)
