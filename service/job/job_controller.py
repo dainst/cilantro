575bbbf346414e9f427469630aa12cd2c65b0041
Richard Henck <richard.henck@dainst.de>
2018-04-25 11:39:09 +0200

    Code cleanup

--- service/job/job_controller.py
+++ service/job/job_controller.py
@@ -15,8 +15,9 @@ def job_create(job_type, object_id):
     print("created job: %s" % job)
     task = job.run()
     print("created job with id: %s" % task.id)
-    return jsonify({'status': 'Accepted', 'job_id': task.id}), \
-           202, {'Location': url_for('job.job_status', job_id=task.id)}
+    body = jsonify({'status': 'Accepted', 'job_id': task.id})
+    headers = {'Location': url_for('job.job_status', job_id=task.id)}
+    return body, 202, headers
 
 
 @job_controller.route('/<job_id>', methods=['GET'])
