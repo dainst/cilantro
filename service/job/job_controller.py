5a9dd628cf09cec22ca9149dcd64cd2b3814648e
Sebastian Cuy <sebastian.cuy@dainst.de>
2018-04-25 14:03:12 +0200

    add subtask ids to create response
    see #8297

--- service/job/job_controller.py
+++ service/job/job_controller.py
@@ -12,10 +12,17 @@ print("job types: %s" % job_config.job_types)
 def job_create(job_type, object_id):
     params = request.get_json(force=True, silent=True)
     job = job_config.generate_job(job_type, object_id, params)
-    print("created job: %s" % job)
     task = job.run()
     print("created job with id: %s" % task.id)
-    body = jsonify({'status': 'Accepted', 'job_id': task.id})
+
+    job_id = task.id
+    task_ids = _get_task_ids(task)
+
+    body = jsonify({
+        'status': 'Accepted',
+        'job_id': job_id,
+        'task_ids': task_ids
+    })
     headers = {'Location': url_for('job.job_status', job_id=task.id)}
     return body, 202, headers
 
@@ -29,3 +36,11 @@ def job_status(job_id):
     if hasattr(task.info, 'result'):
         response['result'] = task.info['result']
     return jsonify(response)
+
+
+def _get_task_ids(task):
+    task_ids = []
+    while task.parent is not None:
+        task_ids.insert(0, task.id)
+        task = task.parent
+    return task_ids
