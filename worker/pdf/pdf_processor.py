f428ddfe29b57faa4e499ce4f90940a2e842412b
ngrippa <nemo.grippa@dainst.de>
2018-05-02 16:36:20 +0200

    some refactoring

--- worker/pdf/pdf_processor.py
+++ worker/pdf/pdf_processor.py
@@ -4,29 +4,16 @@ import os
 def cut_pdf(data):
     articles = data['articles']
     for nr, article in enumerate(articles):
-        (start, end) = _start_end(article['pages'])
         try:
-            output = _set_output(data, article, nr)
-            files = {
-                "file": article['filepath'],
-                "from": start,
-                "to": end,
-                "absolute": True
-            }
+            article['filepath']
         except NameError:
             print("Article without file")
         else:
-            file_list = [files]
-            try:
-                attached = article['attached']
-            except NameError:
-                pass
-            else:
-                for attached_file in attached:
-                    file_list.append(attached_file)
-            merge_str = _merge_pdf_string(file_list)
+            files = _set_files(article)
+            merge_str = _merge_pdf_string(files)
+            output = _set_output(data, article, nr)
             shell = f"pdftk {merge_str} output {os.environ['WORKING_DIR']}/{output} 2>&1"
-            print(f"Excecuting shell command {shell}")
+            print(f"Excecuting shell command {shell}, but pdftk is not installed yet so we dont really excecute it")
             # pdftk = os.system(shell)
             # if not (pdftk == ''):
             #     raise Exception(f"Pdftk occured an error using {shell}, it returns {pdftk}.")
@@ -79,4 +66,24 @@ def _merge_pdf_string(files):
     handle_def = " ".join(handle_def)
     cut_def = " ".join(cut_def)
 
-    return (f"{handle_def} cat {cut_def}")
+    return f"{handle_def} cat {cut_def}"
+
+def _set_files(article):
+    (start, end) = _start_end(article['pages'])
+    root_file = {
+        "file": article['filepath'],
+        "from": start,
+        "to": end,
+        "absolute": True
+    }
+    files = [root_file]
+    try:
+        attached = article['attached']
+    except NameError:
+        # no attached files, we can return only the root file
+        pass
+    else:
+        for attached_file in attached:
+            files.append(attached_file)
+
+    return files
