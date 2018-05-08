fb7fa6345e443b9061cd56c41472295f7a5a0bb1
Sebastian Cuy <sebastian.cuy@dainst.de>
2018-05-08 16:40:05 +0200

    use custom logging conf for celery

--- worker/tasks.py
+++ worker/tasks.py
@@ -1,8 +1,15 @@
+import celery.signals
 from utils.setup_logging import setup_logging
 from utils.celery_client import celery_app
 
 
 setup_logging()
+
+
+@celery.signals.setup_logging.connect
+def on_celery_setup_logging(**kwargs):
+    pass
+
 
 celery_app.autodiscover_tasks([
     'worker.convert',
