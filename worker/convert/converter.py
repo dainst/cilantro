7e5d825aad9094e8805217f01f38aebab883bcba
Sebastian Cuy <sebastian.cuy@dainst.de>
2018-05-08 16:22:00 +0200

    setup logging for workers

--- worker/convert/converter.py
+++ worker/convert/converter.py
@@ -1,6 +1,9 @@
+import logging
 from PIL import Image
 
 
 def convert_tif2jpg(source_file, target_file):
     if source_file != target_file:
+        logging.getLogger(__name__).debug("Converting %s to %s", source_file,
+                                          target_file)
         Image.open(source_file).save(target_file)
--- worker/tasks.py
+++ worker/tasks.py
@@ -1,5 +1,8 @@
+from utils.setup_logging import setup_logging
 from utils.celery_client import celery
 
+
+setup_logging()
 
 celery.autodiscover_tasks([
     'worker.convert',
