748d7694e103679b79f6e3ef9efd6d44658e9880
Sebastian Cuy <sebastian.cuy@dainst.de>
2018-05-08 16:07:02 +0200

    rename utils.logging to utils.setup_logging
    this circumvents import errors that occurred when running tests in IntelliJ

--- run_service.py
+++ run_service.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 from flask import Flask
-from utils.logging import setup_logging
+from utils.setup_logging import setup_logging
 from service.front_controller import front_controller
 from service.job.job_controller import job_controller
 
