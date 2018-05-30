cb309f9861dfc0675f9b45c9c16df562957e121f
Sebastian Cuy <sebastian.cuy@dainst.de>
2018-05-30 14:04:23 +0200

    adjust line length

--- test/service/job/test_job_config.py
+++ test/service/job/test_job_config.py
@@ -4,7 +4,8 @@ import os
 
 from celery.canvas import Signature
 
-from service.job.job_config import JobConfig, ConfigParseException, UnknownJobTypeException
+from service.job.job_config import JobConfig, ConfigParseException,\
+    UnknownJobTypeException
 
 
 class JobConfigTest(unittest.TestCase):
@@ -14,7 +15,10 @@ class JobConfigTest(unittest.TestCase):
 
         job_config = JobConfig()
         job1 = job_config.generate_job("job1", "foo").chain
-        self.assertTrue(isinstance(job1, Signature), "job1 is an instance of '%s', expected 'Signature'" % type(job1))
+        self.assertTrue(
+            isinstance(job1, Signature),
+            "job1 is an instance of '%s', expected 'Signature'" % type(job1)
+        )
         self.assertEqual('celery.chain', job1['task'])
 
         tasks = job1.tasks
@@ -24,18 +28,22 @@ class JobConfigTest(unittest.TestCase):
         self.assertEqual('foo', tasks[0]['kwargs']['object_id'])
 
         self.assertEqual('foreach', tasks[1]['task'])
-        self.assertEqual('foo', tasks[1]['kwargs']['object_id'])
-        self.assertEqual('retrieve', tasks[1]['kwargs']['prev_task'])
-        self.assertEqual('*.tif', tasks[1]['kwargs']['pattern'])
-        self.assertEqual('convert', tasks[1]['kwargs']['subtasks'][0]['name'])
-        self.assertEqual('high', tasks[1]['kwargs']['subtasks'][0]['params']['quality'])
+        kwargs = tasks[1]['kwargs']
+        self.assertEqual('foo', kwargs['object_id'])
+        self.assertEqual('retrieve', kwargs['prev_task'])
+        self.assertEqual('*.tif', kwargs['pattern'])
+        self.assertEqual('convert', kwargs['subtasks'][0]['name'])
+        self.assertEqual('high', kwargs['subtasks'][0]['params']['quality'])
 
         self.assertEqual('publish', tasks[2]['task'])
         self.assertEqual('foo', tasks[2]['kwargs']['object_id'])
         self.assertEqual('foreach', tasks[2]['kwargs']['prev_task'])
 
         job2 = job_config.generate_job("job2", "bar").chain
-        self.assertTrue(isinstance(job2, Signature), "job2 is an instance of '%s', expected 'Signature'" % type(job1))
+        self.assertTrue(
+            isinstance(job2, Signature),
+            "job2 is an instance of'%s', expected 'Signature'" % type(job1)
+        )
         self.assertEqual('celery.chain', job2['task'])
 
         tasks = job2.tasks
@@ -47,12 +55,13 @@ class JobConfigTest(unittest.TestCase):
     def test_unknown_job_type(self):
         os.environ['CONFIG_DIR'] = "test/resources/configs/config_valid"
         job_config = JobConfig()
-        self.assertRaises(UnknownJobTypeException, job_config.generate_job, "job3", "foo")
+        self.assertRaises(UnknownJobTypeException, job_config.generate_job,
+                          "job3", "foo")
 
     def test_invalid_yaml(self):
         os.environ['CONFIG_DIR'] = "test/resources/configs/config_invalid_yaml"
         self.assertRaises(ConfigParseException, JobConfig)
 
     def test_invalid_definition(self):
-        os.environ['CONFIG_DIR'] = "test/resources/configs/config_invalid_definition"
+        os.environ['CONFIG_DIR'] = "test/resources/configs/config_invalid_def"
         self.assertRaises(ConfigParseException, JobConfig)
