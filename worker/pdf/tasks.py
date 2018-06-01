b43b79efe278371c4356426eafa173135ef17a52
Richard Henck <richard.henck@dainst.de>
2018-06-01 16:58:26 +0200

    Split-PDF task now copies all the data from the source to target dir before working

--- worker/pdf/tasks.py
+++ worker/pdf/tasks.py
@@ -1,5 +1,7 @@
 import os
 import json
+import shutil
+
 from worker.pdf.pdf import cut_pdf
 from utils.celery_client import celery_app
 
@@ -8,9 +10,12 @@ from utils.celery_client import celery_app
 def task_split_pdf(object_id, job_id, prev_task):
     source = os.path.join(os.environ['WORKING_DIR'], job_id, object_id, prev_task)
     target = os.path.join(os.environ['WORKING_DIR'], job_id, object_id, 'split_pdf')
-    json_path = os.path.join(source, 'data_json/data.json')
-    os.mkdir(target)
+
+    shutil.copytree(source, target)
+
+    json_path = os.path.join(target, 'data_json/data.json')
+
     with open(json_path) as data_object:
         data = json.load(data_object)
 
-    cut_pdf(data, source, target)
+    cut_pdf(data, target, target)
