bc535c03fad6b6f1fad75e217f12439e8b7a85ca
paflov <philipp.franck@dainst.de>
2018-06-01 13:51:40 +0200

    use port 3333 instead of 3000

--- test/e2e/mock-backend/backend.js
+++ test/e2e/mock-backend/backend.js
@@ -6,7 +6,7 @@ const fakeRest = require('./fake-rest');
 const server = jsonServer.create();
 const router = jsonServer.router('routes.json');
 const middleWares = jsonServer.defaults();
-const port = 3000;
+const port = 3333;
 
 server.use(bodyParser.urlencoded({extended: false}));
 server.use(bodyParser.json());
