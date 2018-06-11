b761788076e951a5bad1c4f39c4b45bb014d59c2
Sebastian Cuy <sebastian.cuy@dainst.de>
2018-06-11 17:46:49 +0200

    Rename tif to jpg method and task
    in order to better match the general naming conventions

--- test/worker/convert/test_tif2jpg.py
+++ test/worker/convert/test_tif2jpg.py
@@ -3,26 +3,26 @@ import unittest
 import logging
 
 from pathlib import Path
-from worker.convert.converter import convert_tif2jpg
+from worker.convert.converter import convert_tif_to_jpg
 
 
 log = logging.getLogger(__name__)
 
 
-class Tif2JpgTest(unittest.TestCase):
+class TifToJpgTest(unittest.TestCase):
 
     tif_path = 'test/resources/objects/some_tiffs/test.tif'
     broken_tif_path = 'test/resources/objects/test_object/image1.tif'
     jpg_path = 'test/resources/objects/some_tiffs/test.jpg'
 
     def test_success(self):
-        convert_tif2jpg(self.tif_path, self.jpg_path)
+        convert_tif_to_jpg(self.tif_path, self.jpg_path)
         self.assertTrue(Path(self.jpg_path).is_file())
         stat = os.stat(self.jpg_path)
         self.assertGreater(stat.st_size, 0)
 
     def test_error(self):
-        self.assertRaises(OSError, convert_tif2jpg, self.broken_tif_path, self.jpg_path)
+        self.assertRaises(OSError, convert_tif_to_jpg, self.broken_tif_path, self.jpg_path)
 
     @classmethod
     def tearDownClass(cls):
