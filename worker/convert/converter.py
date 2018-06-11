38d2da12ef3fa0e7f60ea5901fdf6672607583b2
njunghans <junghans.nino@googlemail.com>
2018-06-11 17:26:52 +0200

    Rework using pdftotext instead of PyPDF2
     - changed docker pipenv files to use pdftotext

--- worker/convert/converter.py
+++ worker/convert/converter.py
@@ -1,6 +1,6 @@
 import logging
 from PIL import Image
-import PyPDF2
+import pdftotext
 
 
 def convert_tif2jpg(source_file, target_file):
@@ -12,9 +12,10 @@ def convert_tif2jpg(source_file, target_file):
 
 def convert_pdf2txts(source_file, output_dir):
     with open(source_file, "rb") as input_stream:
-        pdf = PyPDF2.PdfFileReader(input_stream)
-
-        for index in range(0, pdf.getNumPages()):
+        pdf = pdftotext.PDF(input_stream)
+        index = 0
+        for page in pdf:
             output = open('%s%d.txt' % (output_dir, index), 'wb')
-            output.write(pdf.getPage(index).extractText().encode('utf-8'))
+            output.write(page.encode('utf-8'))
             output.close()
+            index = index + 1
