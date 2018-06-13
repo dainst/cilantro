d8cc7b776adcc387f1f0edc2f3e5941520fb17fb
Nemo Grippa <nemo.grippa@dainst.de>
2018-06-13 14:00:32 +0200

    removed unused BaseClass and Celery Logging from nlp_worker.tasks

--- nlp_worker/tasks.py
+++ nlp_worker/tasks.py
@@ -1,24 +1,5 @@
-import os
-
-import celery.signals
-from celery.task import Task
-
 from utils.celery_client import celery_app
-
-@celery.signals.setup_logging.connect
-def on_celery_setup_logging(**kwargs):
-    pass
-
-
-class BaseTask(Task):
-
-    working_dir = os.environ['WORKING_DIR']
-
-    def get_work_path(self, job_id):
-        return os.path.join(self.working_dir, job_id)
-
 
 celery_app.autodiscover_tasks([
     'nlp_worker.annotate'
 ], force=True)
-
