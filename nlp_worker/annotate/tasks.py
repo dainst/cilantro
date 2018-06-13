2ad9fd81bdfabddff9cc6856f82d91d3064935b1
Nemo Grippa <nemo.grippa@dainst.de>
2018-06-13 14:19:13 +0200

    Uses now class structure instead of celery tasks

--- nlp_worker/annotate/tasks.py
+++ nlp_worker/annotate/tasks.py
@@ -7,27 +7,39 @@ from worker.tasks import BaseTask
 from nlp_worker.annotate.annotate import annotate, get_entities
 
 
-@celery_app.task(bind=True, name="annotate", base=BaseTask)
-def task_annotate(self, object_id, job_id):
-    work_path = self.get_work_path(job_id)
-    json_path = os.path.join(work_path, 'text.json')
-    with open(json_path) as data_object:
-        data = json.load(data_object)
+class AnnotateTask(BaseTask):
 
-    self.update_state(state='PROGRESS',
-                      meta={'status': 'Running NLP analysis...'})
-    result = annotate(data['text'], data['params'])
-    print(result)
+    name = "annotate"
+
+    def execute_task(self):
+        work_path = self.get_work_path(self.job_id)
+        json_path = os.path.join(work_path, 'text.json')
+        with open(json_path) as data_object:
+            data = json.load(data_object)
+
+        self.update_state(state='PROGRESS',
+                          meta={'status': 'Running NLP analysis...'})
+        result = annotate(data['text'], data['params'])
+        print(result)
 
 
-@celery_app.task(bind=True, name="get_enteties", base=BaseTask)
-def task_get_entities(self, object_id, job_id):
-    work_path = self.get_work_path(job_id)
-    json_path = os.path.join(work_path, 'text.json')
-    with open(json_path) as data_object:
-        data = json.load(data_object)
+AnnotateTask = celery_app.register_task(AnnotateTask())
 
-    self.update_state(state='PROGRESS',
-                      meta={'status': 'Running NLP analysis...'})
-    result = get_entities(data['text'], data['params'])
-    print(result)
+
+class GetEntetiesTask(BaseTask):
+
+    name = "get_enteties"
+
+    def execute_task(self):
+        work_path = self.get_work_path(self.job_id)
+        json_path = os.path.join(work_path, 'text.json')
+        with open(json_path) as data_object:
+            data = json.load(data_object)
+
+        self.update_state(state='PROGRESS',
+                          meta={'status': 'Running NLP analysis...'})
+        result = get_entities(data['text'], data['params'])
+        print(result)
+
+
+GetEntetiesTask = celery_app.register_task(GetEntetiesTask())
