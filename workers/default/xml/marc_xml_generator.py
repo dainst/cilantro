2879476d70a99e236f6dd48bbfeca7c6c80a5fe7
Richard Henck <richard.henck@dainst.de>
2018-06-14 11:16:40 +0200

    Put Marc XML in separate folder per article
    Also adjusted the unit tests.
    
    This is to pronounce the structure of journals and articles. Also makes it
    easier to validate all marc XMLs.

--- workers/default/xml/marc_xml_generator.py
+++ workers/default/xml/marc_xml_generator.py
@@ -3,15 +3,16 @@ from workers.default.xml.xml_generator import generate_xml
 
 def generate_marc_xml(work_path, data, template):
     """
-    Merges the article data with the journal data for all articles. Then calls the generic generat_xml function
-    for all articles.
-    :param work_path: Path to put the generated XML
-    :param data: Journal metadata
-    :param template: Marc-XML template
+    Merges article data with the journal data, then calls the
+    generic generate_xml function for all articles.
+    :param str work_path: Path to put the generated XML
+    :param dict data: Journal metadata
+    :param str template: Marc-XML template
     :return: None
     """
     for article in data['articles']:
         generate_xml(work_path,
                      {**data['data'], **article},
                      template,
-                     article['title'].replace(' ', '_') + ".xml")
+                     'articles/' + article['title'].replace(' ', '_'),
+                     "marc.xml")
