21bfdd96e54c37bf32bb3086c296fc00885210fb
Sebastian Cuy <sebastian.cuy@dainst.de>
2018-06-14 16:04:50 +0200

    Change data.json location
    The JSON metadata file is now expected to be at the root of the job
    directory in the workspace.

--- workers/convert/pdf/tasks.py
+++ workers/convert/pdf/tasks.py
@@ -12,7 +12,7 @@ class SplitPdfTask(BaseTask):
 
     def execute_task(self):
         work_path = self.get_work_path(self.job_id)
-        json_path = os.path.join(work_path, 'data_json/data.json')
+        json_path = os.path.join(work_path, 'data.json')
         with open(json_path, 'r') as data_object:
             data = json.load(data_object)
         data = cut_pdf(data, work_path, work_path)
--- workers/default/metadata/loader.py
+++ workers/default/metadata/loader.py
@@ -3,7 +3,7 @@ import os
 
 
 def load_metadata(path):
-    json_path = os.path.join(path, 'data_json/data.json')
+    json_path = os.path.join(path, 'data.json')
     with open(json_path) as data_object:
         data = json.load(data_object)
     return data
