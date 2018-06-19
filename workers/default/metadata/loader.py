b214d4e06b9518c1d8623260121f8607dcd65da0
Sebastian Cuy <sebastian.cuy@uni-koeln.de>
2018-06-19 09:27:43 +0200

    Add write metadata task
    Also adds the corresponding service method.

--- workers/default/metadata/loader.py
+++ workers/default/metadata/loader.py
@@ -2,8 +2,17 @@ import json
 import os
 
 
+_json_file = 'data.json'
+
+
 def load_metadata(path):
-    json_path = os.path.join(path, 'data.json')
+    json_path = os.path.join(path, _json_file)
     with open(json_path) as data_object:
         data = json.load(data_object)
     return data
+
+
+def write_metadata(path, metadata):
+    json_path = os.path.join(path, _json_file)
+    with open(json_path, 'w') as outfile:
+        json.dump(metadata, outfile)
