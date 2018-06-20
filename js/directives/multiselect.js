6b2cfd89419bade1df1abe49e8ae221108cabb12
paflov <philipp.franck@dainst.de>
2018-06-20 14:22:32 +0200

    formal refactoring

--- js/directives/multiselect.js
+++ js/directives/multiselect.js
@@ -1,23 +1,19 @@
-/**
- * Created by pfranck on 19.04.17.
- */
-
 angular
-	.module('directive.multiselect', ['ng'] )
-	.directive('multiselect' , ['$log', function ($log) {
-		return {
-			restrict: 'E',
-			transclude: true,
-			scope: {
-				'elements': '='
-			},
-			templateUrl: 'partials/element-multiselect.html',
-			link: function(scope, element, attrs) {
-				scope.toggled = true;
-				scope.toggleList  = function() {
-					scope.toggled = !scope.toggled;
-				}
+.module('directive.multiselect', ['ng'] )
+.directive('multiselect' , ['$log', function ($log) {
+	return {
+		restrict: 'E',
+		transclude: true,
+		scope: {
+			'elements': '='
+		},
+		templateUrl: 'partials/element-multiselect.html',
+		link: function(scope, element, attrs) {
+			scope.toggled = true;
+			scope.toggleList  = function() {
+				scope.toggled = !scope.toggled;
 			}
-
 		}
-	}]);
+
+	}
+}]);
