bc28b44d098865ec19d0841c7bbe8dc0ed9be530
Sebastian Cuy <sebastian.cuy@dainst.de>
2018-06-20 16:20:21 +0200

    Add docstrings for staging_controller

--- service/staging/staging_controller.py
+++ service/staging/staging_controller.py
@@ -12,22 +12,34 @@ ALLOWED_EXTENSIONS = set(['xml', 'pdf', 'tif', 'tiff', 'json'])
 
 @staging_controller.route('', methods=['GET'], strict_slashes=False)
 def list_staging():
+    """
+    List files in the staging area
+    :return list(str):
+    """
     return jsonify(os.listdir(staging_dir))
 
 
 @staging_controller.route('', methods=['POST'], strict_slashes=False)
 def upload_to_staging():
+    """
+    Upload files to the staging area
+
+    The upload endpoint is able to handle single files that were posted
+    under the 'file' key as well as multiple files at once that have been
+    posted under the 'files' key.
+    :return dict:
+    """
     if 'file' in request.files:
         file = request.files['file']
         if file and _allowed_file(file.filename):
             _upload_file(file)
-            return jsonify({'success': True}), 200
+            return jsonify({'success': True})
     elif 'files' in request.files:
         files = request.files.getlist("files")
         for file in files:
             if file and _allowed_file(file.filename):
                 _upload_file(file)
-        return jsonify({'success': True}), 200
+        return jsonify({'success': True})
     return jsonify({'success': False}), 400
 
 
