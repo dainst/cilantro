345893141c5e2c84be5c9225d1bd197079dea9d7
Theresa <hradilak@uni-potsdam.de>
2018-06-21 18:10:56 +0200

    enabled Upload with Cilantro, basic function works, error handling etc. remains to be done

--- js/controllers/upload.js
+++ js/controllers/upload.js
@@ -24,69 +24,31 @@ angular.module('controller.upload', ['ngFileUpload'])
 			console.log(paths);
 		}
 
-		messenger.ok();
-
 		if (!files || !files.length) {
 			$scope.invalidFiles = true;
 			return;
 		}
 
 		Upload.upload({
-			url: settings.server_url,
-			data: {
-				task: uploadTask || "upload",
-				files: files,
-				path: paths
-			}
-
-		}).then(
-		function uploadSuccess(response) {
-			$scope.progress = 0;
-
-			if (typeof response.data === "string") {
-				messenger.message(response.data, true);
-				return;
-			}
-
-			messenger.cast(response.data);
-
-			if (response.data.success === false) {
-				return;
-			}
-
-			$scope.result = response.data;
-
-			$scope.uploadedFiles = $scope.uploadedFiles.concat(response.data.uploadedFiles);
-
-			if (typeof repository.repository !== "undefined") { // because this controller is also used in context fo csv dialogue
-				repository.update(response.data.repository);
-				dataset.data.importFilePath = response.data.uploadedFiles[0];
-			}
-
-			if (angular.isFunction(callback)) {
-				callback(response)
-			}
-
-			$scope.invalidFiles = false;
-
-          	// server error
-      },
-      function uploadError(response) {
-      	$scope.progress = 0;
-      	if (response.status > 0) {
-      		messenger.message(response.data, true);
-      	}
-          // progress
-      }, function uploadProgress(evt) {
-      	$scope.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
-      });
+			url: 'http://localhost:5000/staging',
+			method: 'POST',
+			headers: {"Content-Type" : 'multipart/form-data'},
+			data:files,
 
 
-	};
-
+		}).success(function(data, status, headers, config){
+			if (data.success){
+				console.log("Heureka!");
+			}; 
+		}).error(function(data, status, headers, config){
+			console.log("An error occurred."); //get HTTP Status Code in here somehow
+		});
+ 
 	$scope.uploadCSV = function(files, callback) {
 		$scope.uploadFiles(files, 'uploadCSV', callback)
 	}
+}
+
 }]).filter('trustHtml', function($sce) {
 	return function(val) {
 		return $sce.trustAsHtml(val);
