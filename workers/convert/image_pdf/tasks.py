aa62f660fbd099f2245888996390423164cca4b4
Nemo Grippa <nemo.grippa@dainst.de>
2018-06-27 17:59:15 +0200

    Nlp Task now performs for every txt

--- workers/convert/image_pdf/tasks.py
+++ workers/convert/image_pdf/tasks.py
@@ -49,7 +49,10 @@ class PdfToTxtTask(BaseTask):
 
     def execute_task(self):
         file = self.get_param('file')
-        convert_pdf_to_txt(file, self.get_work_path())
+        folder, _ = os.path.splitext(file)
+        folder += '_nlp'
+        os.makedirs(folder)
+        convert_pdf_to_txt(file, folder)
 
 
 class PdfToTifTask(BaseTask):
--- workers/nlp/annotate/tasks.py
+++ workers/nlp/annotate/tasks.py
@@ -21,19 +21,24 @@ class AnnotateTask(BaseTask):
 
     def execute_task(self):
         work_path = self.get_work_path()
-        json_path = os.path.join(work_path, 'nlp_params.json')
-        text_path = os.path.join(work_path, 'nlp_text.txt')
-
-        with open(json_path) as data_object:
-            try:
-                params = json.load(data_object)
-            except json.decoder.JSONDecodeError:
-                params = {}
-        with open(text_path, 'r') as file:
+        json_file = os.path.join(work_path, 'nlp_params.json')
+        text_file = self.get_param('file')
+        _, extension = os.path.splitext(text_file)
+        output_file = text_file.replace(extension, '.json')
+        try:
+            with open(json_file, 'r') as data_object:
+                try:
+                    params = json.load(data_object)
+                except json.decoder.JSONDecodeError:
+                    params = {}
+        except FileNotFoundError:
+            raise Exception("No JSON FILE found")
+        with open(text_file, 'r') as file:
             text = file.read().replace('\n', '')
 
-        result = annotate(text, params)
-        with open(os.path.join(work_path, 'annotations.json'), 'w') as file:
+        # result = annotate(text, params)
+        result = "Disabled due to runtime"
+        with open(output_file, 'w+') as file:
             json.dump(result, file)
 
 
