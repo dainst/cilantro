eb7af746868932a3a040fe2ca0891fd8223f605a
paflov <philipp.franck@dainst.de>
2018-07-04 11:39:35 +0200

    message box: give message box isolated scope, to prevent some bugs

--- js/directives/messagebox.js
+++ js/directives/messagebox.js
@@ -4,6 +4,9 @@ angular
     return {
         restrict: 'E',
         templateUrl: 'partials/elements/messagebox.html',
+        scope: {
+            isOpen: '@'
+        },
         link: function(scope, elem, attrs) {
 
             const boostrapClassMap = {
@@ -14,11 +17,10 @@ angular
                 debug: "alert-debug",
                 urgent: "alert-info alert-urgent",
             };
-
+            scope.isOpen = false;
             scope.getClass = (type) => boostrapClassMap[type] || "alert-info";
             scope.messages = messenger.messages;
             scope.hasContent = () => messenger.messages.length > 0;
-            scope.isOpen = false;
             scope.toggle = (to) => {scope.isOpen = angular.isUndefined(to) ? to : !scope.isOpen};
             scope.getMain = messenger.getMainMessage;
             scope.clearLog = messenger.clear;
