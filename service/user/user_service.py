b6c1aba576a7d29c1cf983eb122fae0ca3f86dca
Sebastian Cuy <sebastian.cuy@dainst.de>
2018-07-10 16:04:44 +0200

    Store passwords encrypted with bcrypt

--- service/user/user_service.py
+++ service/user/user_service.py
@@ -1,16 +1,19 @@
 import os
 import yaml
+import bcrypt
 from flask_httpauth import HTTPBasicAuth
 
 auth = HTTPBasicAuth()
 
 _users_config = os.path.join(os.environ['CONFIG_DIR'], "users.yml")
-_users_file = open(_users_config, 'r')
+_users_file = open(_users_config, 'r', encoding="utf-8")
 users = yaml.load(_users_file)
 
 
-@auth.get_password
-def get_password(username):
+@auth.verify_password
+def verify_password(username, password):
     if username in users:
-        return users.get(username)['password']
-    return None
+        hashed_password = users.get(username)['password'].encode('utf-8')
+        return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+    return False
+
