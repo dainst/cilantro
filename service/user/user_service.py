cbd06463b159005574280ee09872dfb62700b0f0
Sebastian Cuy <sebastian.cuy@dainst.de>
2018-07-10 13:31:40 +0200

    Check against users config file in user service

--- service/user/user_service.py
+++ service/user/user_service.py
@@ -1,14 +1,16 @@
+import os
+import yaml
 from flask_httpauth import HTTPBasicAuth
 
 auth = HTTPBasicAuth()
 
-users = {
-    "admin": "s3cr3t"
-}
+_users_config = os.path.join(os.environ['CONFIG_DIR'], "users.yml")
+_users_file = open(_users_config, 'r')
+users = yaml.load(_users_file)
 
 
 @auth.get_password
 def get_password(username):
     if username in users:
-        return users.get(username)
+        return users.get(username)['password']
     return None
