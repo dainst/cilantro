1f7c44f8420fdb9712ad0ad3fcfef28db348802d
Richard Henck <richard.henck@dainst.de>
2018-07-11 17:54:24 +0200

    Add test for tif_to_txt with wrong langiage passed
    The test image is in english and when given german as language there
    is a slight mistake (oven->even).

--- test/worker/convert/test_tiff_to_text.py
+++ test/worker/convert/test_tiff_to_text.py
@@ -6,7 +6,7 @@ from workers.convert.tiff_to_text import tif_to_txt
 
 class GenerateTextFromTiff(unittest.TestCase):
 
-    def test_tiff_to_text(self):
+    def test_tif_to_txt(self):
         resource_dir = os.environ['RESOURCE_DIR']
         working_dir = os.environ['WORKING_DIR']
         tiff_path = f'{resource_dir}/files/test.tif'
@@ -23,3 +23,21 @@ class GenerateTextFromTiff(unittest.TestCase):
                          "When you're about to ﬁght the\n"
                          "Persians but you remember\n"
                          "that you left the oven on")
+
+    def test_tif_to_txt_wrong_language(self):
+        resource_dir = os.environ['RESOURCE_DIR']
+        working_dir = os.environ['WORKING_DIR']
+        tiff_path = f'{resource_dir}/files/test.tif'
+        target_file_path = f'{working_dir}/test.converted.jpg'
+
+        tif_to_txt(tiff_path, target_file_path, language='deu')
+
+        self.assertTrue(os.path.isfile(os.path.join(working_dir, target_file_path)))
+
+        with open(target_file_path, 'r') as file:
+            result_txt = file.read()
+
+        self.assertNotEqual(result_txt,
+                            "When you're about to ﬁght the\n"
+                            "Persians but you remember\n"
+                            "that you left the oven on")
