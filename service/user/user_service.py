79c445114a40824d96e6160750701359a54abd00
Sebastian Cuy <sebastian.cuy@dainst.de>
2018-07-11 11:58:54 +0200

    Use yaml.safe_load() to load user config

--- service/user/user_service.py
+++ service/user/user_service.py
@@ -7,7 +7,7 @@ auth = HTTPBasicAuth()
 
 _users_config = os.path.join(os.environ['CONFIG_DIR'], "users.yml")
 _users_file = open(_users_config, 'r', encoding="utf-8")
-users = yaml.load(_users_file)
+users = yaml.safe_load(_users_file)
 
 
 @auth.verify_password
