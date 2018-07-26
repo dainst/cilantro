b0678faa7744cb3e8a9d410037790d03c22f07b2
njunghans <junghans.nino@googlemail.com>
2018-07-26 14:35:19 +0200

    Fix assertRaises wrong semantic

--- test/service/job/test_job_config.py
+++ test/service/job/test_job_config.py
@@ -50,13 +50,13 @@ class JobConfigTest(unittest.TestCase):
                           "job3", "test_user")
 
     def test_invalid_yaml(self):
-        self.assertRaises(ConfigParseException, JobConfig("test/resources/configs/config_invalid_yaml"))
+        self.assertRaises(ConfigParseException, JobConfig, "test/resources/configs/config_invalid_yaml")
 
     def test_invalid_definition(self):
-        self.assertRaises(ConfigParseException, JobConfig("test/resources/configs/config_invalid_def"))
+        self.assertRaises(ConfigParseException, JobConfig, "test/resources/configs/config_invalid_def")
 
     def test_invalid_params(self):
-        self.assertRaises(ConfigParseException, JobConfig("test/resources/configs/config_invalid_param"))
+        self.assertRaises(ConfigParseException, JobConfig, "test/resources/configs/config_invalid_param")
 
     def test_config_param_in_task(self):
         job_config = JobConfig("test/resources/configs/config_params")
