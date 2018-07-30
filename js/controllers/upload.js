1581443f75987a4748ff0e50e58eb4dfbee56653
paflov <philipp.franck@dainst.de>
2018-07-30 17:27:52 +0200

    upload files with credentials

--- js/controllers/upload.js
+++ js/controllers/upload.js
@@ -1,78 +1,79 @@
 angular.module('controller.upload', ['ngFileUpload'])
 
 .controller('upload', ['$scope', '$rootScope','Upload', '$timeout', 'settings', 'webservice', 'messenger', 'staging_dir',
-	function ($scope, $rootScope, Upload, $timeout, settings, webservice, messenger, staging_dir) {
+    function ($scope, $rootScope, Upload, $timeout, settings, webservice, messenger, staging_dir) {
 
+        $scope.uploadedFiles = [];
+        $scope.uploadedFoldersAndFiles = [];
 
-		$scope.uploadedFiles = [];
-		$scope.uploadedFoldersAndFiles = [];
+        function updateUploadedFoldersAndFiles(files){
+            let folders = [];
+            files.forEach(function(file){
+                if (file.path){
+                    let folderName = file.path.split('/',1)[0];
+                    if (!folders.includes(folderName)){
+                        folders.push(folderName);
+                        $scope.uploadedFoldersAndFiles.push("[DIR] " + folderName);
+                    }
+                } else {
+                    $scope.uploadedFoldersAndFiles.push(file.name);
+                }
+            });
+        }
 
-		function updateUploadedFoldersAndFiles(files){
-			let folders = [];
-			files.forEach(function(file){
-				if (file.path){
-					let folderName = file.path.split('/',1)[0];
-					if (!folders.includes(folderName)){
-						folders.push(folderName);
-						$scope.uploadedFoldersAndFiles.push("[DIR] " + folderName);
-					}
-				} else {
-					$scope.uploadedFoldersAndFiles.push(file.name);
-				}
-			});
-		}
+        $scope.dropFile = function(f)  {
+            console.log(f);
+        };
 
-		$scope.dropFile = function(f)  {
-			console.log(f);
-		};
+        $scope.uploadFiles = function(files, uploadTask, callback) {
 
-		$scope.uploadFiles = function(files, uploadTask, callback) {
+            if (!files || !files.length) {
+                $scope.invalidFiles = true;
+                return;
+            }
 
-		if (!files || !files.length) {
-			$scope.invalidFiles = true;
-			return;
-		}
+            const headers = {"Content-Type" : 'multipart/form-data'};
+            if (angular.isDefined(settings.server_user) && angular.isDefined(settings.server_pass)) {
+                headers.Authorization = "Basic " + window.btoa(settings.server_user + ":" + settings.server_pass);
+            }
 
+            //working with ngFileUpload directive
+            Upload.upload({
+                url: settings.files_url,
+                method: 'POST',
+                headers: headers,
+                data: files,
+            }).success(function(data, status, headers, config){
+                $scope.progress = 0;
+                $scope.uploadedFiles = $scope.uploadedFiles.concat(files);
+                updateUploadedFoldersAndFiles(files);
+                $scope.invalidFiles = false;
+                console.log("files", files);
+                webservice.get('staging').then((stagingFolder) => {
+                    console.log("stagingFolder", stagingFolder);
+                    $scope.staging_dir.update(stagingFolder);
+                    $rootScope.$broadcast('refreshView');
+                });
 
-		//working with ngFileUpload directive
-		Upload.upload({
-			url: 'http://localhost:5000/staging',
-			method: 'POST',
-			headers: {"Content-Type" : 'multipart/form-data'},
-			data:files,
+            }).error(function(data, status, headers, config){
+                messenger.error("Upload failed.");
+                $scope.progress = 0;
+                console.log("Upload failed with Status ", status);
+                console.log("Upload headers:", headers);
 
+            }).progress(function(event){
+                $scope.progress = Math.min(100, parseInt(100.0 * event.loaded / event.total));
+            });
+        };
 
-		}).success(function(data, status, headers, config){
-			if (data.success){
-				$scope.progress = 0;
-				$scope.uploadedFiles = $scope.uploadedFiles.concat(files);
-				updateUploadedFoldersAndFiles(files);
-				$scope.invalidFiles = false;
-				console.log("files", files);
-				webservice.get('staging')
-                                .then((stagingFolder) => {
-                                    console.log("stagingFolder", stagingFolder);
-                                    $scope.staging_dir.update(stagingFolder);
-                                    $rootScope.$broadcast('refreshView');
-                                })
-			}; 
-		}).error(function(data, status, headers, config){
-			messenger.error("Upload failed.");
-      		$scope.progress = 0;
-      		console.log("Upload failed with Status ", status);
-      		console.log("Upload headers:", headers);
+        $scope.uploadCSV = function(files, callback) {
+            $scope.uploadFiles(files, 'uploadCSV', callback)
+        }
 
-		}).progress(function(event){
-			$scope.progress = Math.min(100, parseInt(100.0 * event.loaded / event.total));
-		});
- 
-	$scope.uploadCSV = function(files, callback) {
-		$scope.uploadFiles(files, 'uploadCSV', callback)
-	}
-}
+}])
 
-}]).filter('trustHtml', function($sce) {
-	return function(val) {
-		return $sce.trustAsHtml(val);
-	};
+.filter('trustHtml', function($sce) {
+    return function(val) {
+        return $sce.trustAsHtml(val);
+    };
 });
