c188aeac4f93552b4797912de7eae318d5d655cb
Richard Henck <richard.henck@dainst.de>
2018-08-14 17:25:34 +0200

    Move OJS related request-functions to separate lib
    This way we can change the way cilantro talks to OJS
    in one place.

--- workers/default/ojs/frontmatter.py
+++ workers/default/ojs/frontmatter.py
@@ -1,33 +1,17 @@
-import logging
-from urllib.request import Request, urlopen
-
-log = logging.getLogger(__name__)
+from utils.ojs_api import generate_frontmatters
 
 
-def generate_frontmatter(article_id_list, server='ojs', port='80'):
+def generate_frontmatter(article_id_list):
     """
     Generate frontmatter page for articles in OJS via Frontmatter-Plugin.
 
-    Does a simple GET request to the OJS plugin passing on IDs of documents
-    already present in OJSself.
+    The given id_list is merged by commata.
 
-    Authorization is done with a specific test user.
+    For further details see OJS API module.
 
     :param article_id_list: IDs of documents in OJS
-    :param server: address of the OJS server
-    :param port: network port of the OJS server
     :return: tuple containing the response code and text
     """
     ids = ','.join(map(str, article_id_list))
-    headers = {'ojsAuthorization': 'YWRtaW4=:cGFzc3dvcmQ='}
 
-    url_base = "/ojs/plugins/generic/ojs-cilantro-plugin/api/frontmatters/"\
-               "create/article/"
-    url = "http://" + server + ":" + port + url_base + "?id=" + ids
-
-    request = Request(url, headers=headers)
-
-    with urlopen(request) as response:
-        response_text = response.read().decode('utf-8')
-
-    return response.getcode(), response_text
+    return generate_frontmatters(ids)
--- workers/default/ojs/publishing.py
+++ workers/default/ojs/publishing.py
@@ -1,41 +1,12 @@
-import logging
-from urllib.request import Request, urlopen
-
-log = logging.getLogger(__name__)
+from utils.ojs_api import publish
 
 
-def publish_to_ojs(import_xml_file_path, journalcode, server='ojs', port='80'):
+def publish_to_ojs(import_xml_file_path, journalcode):
     """
     Publish the documents referenced in the passed XML via OJS-Import-Plugin.
 
-    The paramter file contains OJS-specific XML which contains file paths to
-    documents which shall be imported to OJS. It is read and passed to a POST
-    request (in UTF-8) to the OJS import plugin.
+    For details see OJS API module.
 
-    Server address and port can be given optionally.
-
-    The Import-Plugin needs authentication which is (for now) hard-coded.
-
-    :param str import_xml_file_path: OJS-Import-XML
-    :param str journalcode: Name of the journal that will be imported to
-    :param server: OJS server address
-    :param port: OJS server port
-    :return: Tuple of return code and text of the POST request to OJS
+    :return: tuple containing the response code and text
     """
-    with open(import_xml_file_path, "r") as f:
-        import_data = f.read()
-
-    headers = {'Content-Type': 'application/xml',
-               'ojsAuthorization': 'YWRtaW4=:cGFzc3dvcmQ='}
-
-    request_url = (f"http://{server}:{port}"
-                   f"/ojs/plugins/generic/ojs-cilantro-plugin/api/import/"
-                   f"{journalcode}")
-
-    request = Request(request_url, headers=headers,
-                      data=import_data.encode(encoding='utf-8'))
-
-    with urlopen(request) as response:
-        response_text = response.read().decode('utf-8')
-
-    return response.getcode(), response_text
+    return publish(import_xml_file_path, journalcode)
