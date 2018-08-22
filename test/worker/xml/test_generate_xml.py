e3a7a2bffebfebb498d33848ba193599798e743c
Richard Henck <richard.henck@dainst.de>
2018-08-22 17:03:29 +0200

    Handle XML generation similarly for issues and articles
    They now share the same task function. A couple of variables will
    be set depending on the type given as parameter.
    
    Task #9210

--- test/worker/xml/test_generate_xml.py
+++ test/worker/xml/test_generate_xml.py
@@ -3,7 +3,6 @@ import os
 import logging
 
 from workers.default.xml.xml_generator import generate_xml
-from workers.default.xml.marc_xml_generator import generate_marc_xml
 from utils.object import Object
 
 log = logging.getLogger(__name__)
@@ -13,20 +12,6 @@ class GenerateXMLTest(unittest.TestCase):
 
         resource_dir = os.environ['RESOURCE_DIR']
         working_dir = os.environ['WORKING_DIR']
-
-        def test_generate_marcxml(self):
-            obj = Object(f'{self.resource_dir}/objects/a_journal')
-
-            template_file = open('resources/marc_template.xml', 'r')
-            template_string = template_file.read()
-            template_file.close()
-
-            generate_marc_xml(obj, template_string, 'test_marcxml.xml')
-
-            self.assertTrue(os.path.isfile(f'{self.resource_dir}/objects/a_journal/parts/part_0001/test_marcxml.xml'))
-            self.assertTrue(os.path.isfile(f'{self.resource_dir}/objects/a_journal/parts/part_0002/test_marcxml.xml'))
-            os.remove(f'{self.resource_dir}/objects/a_journal/parts/part_0001/test_marcxml.xml')
-            os.remove(f'{self.resource_dir}/objects/a_journal/parts/part_0002/test_marcxml.xml')
 
         def test_generate_ojsxml(self):
             obj = Object(f'{self.resource_dir}/objects/a_journal')
@@ -39,14 +24,15 @@ class GenerateXMLTest(unittest.TestCase):
             for part in obj.get_parts():
                 articles_meta.append(part.metadata.to_dict())
 
-            data = {'data': {
-                **obj.metadata.to_dict(),
-                'auto_publish_issue': True,
-                'default_create_frontpage': True,
-            },
+            data = {
+                'metadata': obj.metadata.to_dict(),
+                'ojs_metadata': {
+                    'auto_publish_issue': True,
+                    'default_create_frontpage': True},
                 'articles': articles_meta}
 
             generate_xml(obj.path, data, template_string, 'test_ojsxml.xml')
 
-            self.assertTrue(os.path.isfile(f'{self.resource_dir}/objects/a_journal/test_ojsxml.xml'))
+            self.assertTrue(os.path.isfile(
+                f'{self.resource_dir}/objects/a_journal/test_ojsxml.xml'))
             os.remove(f'{self.resource_dir}/objects/a_journal/test_ojsxml.xml')
