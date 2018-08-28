8bfd2e95b320c9132ad15733c04ef9111c634aaf
Richard Henck <richard.henck@dainst.de>
2018-08-28 12:26:46 +0200

    Convert TIFF to RGB mode before saving as JPEG
    Before it crashed when used in an integration test.

--- workers/convert/convert_image.py
+++ workers/convert/convert_image.py
@@ -13,4 +13,4 @@ def convert_tif_to_jpg(source_file, target_file):
     if source_file != target_file:
         logging.getLogger(__name__).debug(f"Converting {source_file} "
                                           f"to {target_file}")
-        PilImage.open(source_file).save(target_file)
+        PilImage.open(source_file).convert('RGB').save(target_file)
