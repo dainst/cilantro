b7c7cae509055181d92d664633aed729bf1eff70
Richard Henck <richard.henck@dainst.de>
2018-08-31 18:02:43 +0200

    Move XML generation logic into the templates
    This cleans up the task function and makes it more generic, i.e.
    usable for all XML types.

--- workers/default/xml/tasks.py
+++ workers/default/xml/tasks.py
@@ -6,7 +6,6 @@ from utils.celery_client import celery_app
 from workers.base_task import ObjectTask
 from workers.default.xml.xml_generator import generate_xml
 from workers.default.xml.xml_validator import validate_xml
-from utils.object import Object
 
 
 log = logging.getLogger(__name__)
@@ -32,40 +31,20 @@ class GenerateXMLTask(ObjectTask):
     name = "generate_xml"
 
     def process_object(self, obj):
-        xml_template_string = _read_file('resources/' + self.get_param('template_file'))
-        target_filename = self.get_param('target_filename')
+        template_file = self.get_param('template_file')
+        target_filepath = os.path.join(obj.path,
+                                       self.get_param('target_filename'))
         try:
             dtd_file = 'resources/' + self.get_param('dtd_file')
         except KeyError:
             dtd_file = None
-            log.debug("No DTD found for XML validation")
         try:
             schema_file = 'resources/' + self.get_param('schema_file')
         except KeyError:
             schema_file = None
-            log.debug("No Schema found for XML validation")
 
-        data = {
-            'metadata': obj.metadata.to_dict()
-            }
-
-        # parts mean that this is an issue, and not an article
-        if any(obj.get_parts()):
-            articles_meta = []
-            # add filepath to article data, which is needed in the XML
-            for part in obj.get_parts():
-                articles_meta.append({
-                    **part.metadata.to_dict(),
-                    'filepath': os.path.join(part.get_representation_dir(
-                                             Object.INITIAL_REPRESENTATION),
-                                             'merged.pdf')  # TODO filename may be different?
-                    })
-
-            data['ojs_metadata'] = self.get_param('ojs_metadata')
-            data['articles'] = articles_meta
-
-        generated_xml_file = generate_xml(obj.path, data, xml_template_string,
-                                          target_filename)
+        generated_xml_file = generate_xml(self, obj, template_file,
+                                          target_filepath)
 
         validate_xml(generated_xml_file, dtd_file_path=dtd_file,
                      schema_file_path=schema_file)
