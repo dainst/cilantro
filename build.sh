8b5e279812785de940157ec2df4b26c958f00ab6
Sebastian Cuy <sebastian.cuy@uni-koeln.de>
2018-08-31 12:16:41 +0200

    Fix paths

--- build.sh
+++ build.sh
@@ -16,8 +16,12 @@ if [ -z "${nocache}" ]
     then
         nocache=""
 fi
+
+cd docker/${name}
 rm Pipfile.lock
 pipenv lock
+cd ../..
+
 docker image build -t ${name}:${tag} -f docker/${name}/Dockerfile . --build-arg GITHUB_ACCESS_TOKEN=${token} ${nocache}
 docker tag ${name}:${tag} dainst/${name}:${tag}
 docker push dainst/${name}:${tag}
