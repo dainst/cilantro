8611079f0e3ec5e610a5a5b36a9438f055347e08
paflov <philipp.franck@dainst.de>
2018-09-13 18:51:24 +0200

    makes in symlink in docker file to prevent a selenium issue with different paths in shared volume between containers

--- docker/cilantro-frontend-test/test.sh
+++ docker/cilantro-frontend-test/test.sh
@@ -3,12 +3,20 @@ cd /salvia
 
 if [ "$TEST" = "skip" ]; then
     echo "Frontend tests skipped"
+    exit 0
 fi
 
 cd /salvia/config
 chmod a+x create_settings_from_env.sh
 ./create_settings_from_env.sh
 cd /salvia
+
+# the selenium tries to find the driver in a path from the other container...
+mkdir /usr/share/nginx
+mkdir /usr/share/nginx/html
+mkdir /usr/share/nginx/html/node_modules
+mkdir /usr/share/nginx/html/node_modules/webdriver-manager
+ln -s /salvia/node_modules/webdriver-manager/selenium /usr/share/nginx/html/node_modules/webdriver-manager/selenium
 
 if [ "$TEST" = "mock" ]; then
     echo "Running Frontend tests against mock-backend"
