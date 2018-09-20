f5eeae18f28463e4226d50e4796eafb38e7e567d
Richard Henck <richard.henck@dainst.de>
2018-09-20 15:03:00 +0200

    Restructure Convert tasks
    Now there are 2 Modules: One for converting PDFs and one for
    converting Images (of any kind).
    
    Since the default workers' 'create_object' task uses
    the PDF-split-and-merge function, which is part of the PDF conversion
    module, the libraries are also needed in that docker image.

--- workers/convert/convert_image.py
+++ workers/convert/convert_image.py
@@ -1,6 +1,9 @@
 import logging
 
 from PIL import Image as PilImage
+import pyocr
+
+log = logging.getLogger(__name__)
 
 
 def convert_tif_to_jpg(source_file, target_file):
@@ -8,9 +11,56 @@ def convert_tif_to_jpg(source_file, target_file):
     Save the parameter source file and saves it as the target file.
 
     :param str source_file: path to the TIF source file
-    :param str output_dir: path to the generated output file
+    :param str target_file: path to the generated output file
     """
     if source_file != target_file:
         logging.getLogger(__name__).debug(f"Converting {source_file} "
                                           f"to {target_file}")
         PilImage.open(source_file).convert('RGB').save(target_file)
+
+
+def convert_jpg_to_pdf(source_file, target_file):
+    """
+    Make a 1 Paged PDF Document from a jpg file.
+
+    :param str source_file: path to the jpg
+    :param str target_file: desired output path
+    """
+    PilImage.open(source_file).save(target_file, 'PDF', resolution=100.0)
+
+
+def tif_to_txt(source_file, target_file, language='eng'):
+    """
+    Extract text from tiff file via OCR and writes to target file.
+
+    Availabe languages can be found here:
+    https://github.com/tesseract-ocr/tesseract/blob/master/doc/tesseract.1.asc#languages
+
+    :param str source_file: file path to tiff
+    :param str target_file: name of generated text-file
+    :param str language: used by tesseract. Possible values: see above.
+    """
+    tools = pyocr.get_available_tools()
+    if len(tools) == 0:
+        log.error("No OCR tool found")
+        return
+    # The tools are returned in the recommended order of usage
+    tool = tools[0]
+    log.debug("Will use ocr-tool: " + tool.get_name())
+
+    langs = tool.get_available_languages()
+    log.debug("Available languages: %s" % ", ".join(langs))
+    if language not in langs:
+        log.error(f'language {language} not available. Defaulting to English.')
+        lang = 'eng'
+    else:
+        lang = language
+    log.debug("Will use lang '%s'" % lang)
+
+    txt = tool.image_to_string(
+        PilImage.open(source_file),
+        lang=lang,
+        builder=pyocr.builders.TextBuilder())
+
+    with open(target_file, 'w') as outfile:
+        outfile.write(txt)
