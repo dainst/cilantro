db4a650708f1ab1582b72c6cafe3cae5c2706aa2
Nino Junghans <junghans.nino@dainst.de>
2018-09-24 09:52:02 +0200

    Change user data in e2e to match backend.

--- frontend/test/e2e/mock-backend/fake_user.js
+++ frontend/test/e2e/mock-backend/fake_user.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 
 module.exports = function fakeUser(req, res, next) {
-    const test_auth = "Basic " + window.btoa('test_username' + ":" + 'test_password');
+    const test_auth = "Basic " + window.btoa('test_user' + ":" + 'test_password');
     if (req.method === "GET") {
         if (req.headers.authorization === test_auth) {
             res.status("200").json({success: true});
