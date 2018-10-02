38bce1ac0fa80b68a2df7d335de59e361d7aa106
Richard Henck <richard.henck@dainst.de>
2018-10-02 14:30:09 +0200

    Code cleanup
    removed some unused stuff, added docstrings etc.

--- test/worker/convert/test_cut_pdf.py
+++ test/worker/convert/test_cut_pdf.py
@@ -2,7 +2,6 @@ import os
 import logging
 
 from test.worker.convert.convert_test import ConvertTest
-
 from workers.convert.convert_pdf import split_merge_pdf
 from utils.object import Object
 
@@ -10,17 +9,18 @@ log = logging.getLogger(__name__)
 
 
 class CutPdfTest(ConvertTest):
-    resource_dir = os.environ['RESOURCE_DIR']
-    working_dir = os.path.join(os.environ['WORKING_DIR'], 'cut_test')
-    pdf_src = f'{resource_dir}/files/test.pdf'
-    file_generated = f'{working_dir}/data/pdf/merged.pdf'
 
-    def test_success(self):
+    def test_cut_pdf(self):
+        """Test cutting PDF into multiple files by page numbers."""
+        pdf_src = f'{self.resource_dir}/files/test.pdf'
+        file_generated = f'{self.working_dir}/data/pdf/merged.pdf'
+
         obj = Object(self.working_dir)
-        params = [{"file": f"{obj.get_representation_dir('pdf')}/test.pdf", "range": [1, 20]},
-                  {"file": f"{obj.get_representation_dir('pdf')}/test.pdf", "range": [21, 27]}]
-        stream = open(self.pdf_src, 'rb')
-        obj.add_stream('test.pdf', 'pdf', stream)
-        stream.close()
+        params = [{"file": "test.pdf", "range": [1, 20]},
+                  {"file": "test.pdf", "range": [21, 27]}]
+
+        with open(pdf_src, 'rb') as stream:
+            obj.add_stream('test.pdf', 'pdf', stream)
+
         split_merge_pdf(params, obj.get_representation_dir('pdf'))
-        self.assertTrue(os.path.isfile(self.file_generated))
+        self.assertTrue(os.path.isfile(file_generated))
