4406e65fa9621c94ebba6404a6a88ed4be314661
Nemo Grippa <nemo.grippa@dainst.de>
2018-10-04 14:24:37 +0200

    Full text is now generated by iterating over representations

--- workers/nlp/annotate/tasks.py
+++ workers/nlp/annotate/tasks.py
@@ -19,37 +19,29 @@ class AnnotateTask(ObjectTask):
     name = "nlp.annotate"
 
     def process_object(self, obj):
-        txt_folder = obj.get_representation_dir(self.get_param('source'))
-        full_text = _get_full_text(txt_folder)
-        json_file = os.path.join(txt_folder, "annotations.json")
+        full_text = self._get_full_text(obj)
+        json_file = os.path.join(obj.get_representation_dir(
+            self.get_param('source')), "annotations.json")
 
         result = annotate(full_text)
         with open(json_file, 'w+') as file:
             json.dump(result, file)
 
+    def _get_full_text(self, obj):
+        """
+        Get the full text from the seperated txts.
+
+        Make it to one big string with new-page markers (\f)in it. This
+        improves both, quality and runtime of the annotations.
+
+        :param class obj: The object
+        :return str: The complete text, merged from all txts
+        """
+        full_text = ""
+        for file2 in obj.get_representation(self.get_param('source')):
+            text = file2.read().decode("UTF-8").replace('\n', '')
+            full_text += f"{text}\f"
+        return full_text
+
 
 AnnotateTask = celery_app.register_task(AnnotateTask())
-
-
-def _get_full_text(folder):
-    """
-    Get the full text from the seperated txts.
-
-    Make it to one big string with new-page markers (\f)in it. This
-    improves both, quality and runtime of the annotations.
-
-    :param str folder: The folder which is searched for the txts
-    :return str: The complete text, merged from all txts
-    """
-    full_text = ""
-    page = 0
-    while True:
-        file = os.path.join(folder, f"merged_{page}.txt")
-        if os.path.isfile(file):
-            with open(file, 'r') as f:
-                text = f.read().replace('\n', '')
-                full_text += f"{text}\f"
-            page += 1
-        else:
-            break
-    return full_text
