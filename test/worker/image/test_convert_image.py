ada9f65396bd7e52652ccc492797af7163e1dd90
Richard Henck <richard.henck@dainst.de>
2018-10-11 17:17:16 +0200

    Fix image scaling tests parameter order and package inclusion.

--- test/worker/image/test_convert_image.py
+++ test/worker/image/test_convert_image.py
@@ -16,16 +16,18 @@ class ConvertImageTest(unittest.TestCase):
 
     def test_scale_jpg(self):
         """Test scaling for JPEG type image."""
-        image_file = os.path.join(self.resource_dir, 'test.jpg')
-        scale_image(image_file, 30, 40, self.resource_dir)
+        image_file = os.path.join(self.resource_dir, 'files', 'test.jpg')
+        scale_image(image_file,
+                    os.path.join(self.resource_dir, 'test_30_40.jpg'), 30, 40)
         self.assertTrue(os.path.isfile(
             f'{self.resource_dir}/test_30_40.jpg'))
         os.remove(f'{self.resource_dir}/test_30_40.jpg')
 
     def test_scale_tif(self):
         """Test scaling for TIFF type image."""
-        image_file = os.path.join(self.resource_dir, 'test.tif')
-        scale_image(image_file, 30, 40, self.resource_dir)
+        image_file = os.path.join(self.resource_dir, 'files', 'test.tif')
+        scale_image(image_file,
+                    os.path.join(self.resource_dir, 'test_30_40.tif'), 30, 40)
         self.assertTrue(os.path.isfile(
             f'{self.resource_dir}/test_30_40.tif'))
         os.remove(f'{self.resource_dir}/test_30_40.tif')
