1ca85aba4be2e348fcbb3c87230a42e2b57df0a9
Nino Junghans <junghans.nino@dainst.de>
2018-10-12 12:42:32 +0200

    Fix publish e2e tests.

--- frontend/test/e2e/specs/publish.spec.js
+++ frontend/test/e2e/specs/publish.spec.js
@@ -3,41 +3,45 @@ const a = require('../modules/actions');
 const so = require('../modules/subobject');
 const mo = require('../modules/mainobject');
 const message = require('../modules/messages');
+const EC = protractor.ExpectedConditions;
 
 
 describe('publish page', () => {
 
-    xit("should not allow start job if no articles are staged", () => {
+    it("should not allow start job if no articles are staged", () => {
         so.goToSubObject(2);
         a.toggleNavbar();
         expect(e.navbar.publish.isDisplayed()).toBeFalsy();
         a.toggleNavbar();
+        browser.wait(EC.visibilityOf(e.subobject.confirmBtn), 2000);
         e.subobject.confirmBtn.click();
         a.toggleNavbar();
         expect(e.navbar.publish.isDisplayed()).toBeTruthy();
     });
 
-    xit("should not allow start job if mainobject data is no valid", () => {
+    it("should not allow start job if mainobject data is no valid", () => {
         so.goToSubObject(2);
+        browser.wait(EC.visibilityOf(e.subobject.confirmBtn), 2000);
         e.subobject.confirmBtn.click();
         expect(e.publish.uploadBtn.isPresent()).toBeFalsy();
         Promise.all([mo.getRowContent("Volume"), mo.getRowContent("Number"), mo.getRowContent("OJS: Journal Code")])
             .then(cells => {
                 cells[0].element(by.css("input")).sendKeys("2018");
                 cells[1].element(by.css("input")).sendKeys("18");
-                cells[2].all(by.css("option")).get(1).click();
+                cells[2].all(by.css("option")).get(0).click();
                 expect(e.publish.uploadBtn.isPresent()).toBeTruthy();
             });
     });
 
-    xit("should start job if everything is okay", () => {
+    it("should start job if everything is okay", () => {
         so.goToSubObject(2);
+        browser.wait(EC.visibilityOf(e.subobject.confirmBtn), 2000);
         e.subobject.confirmBtn.click();
         Promise.all([mo.getRowContent("Volume"), mo.getRowContent("Number"), mo.getRowContent("OJS: Journal Code")])
             .then(cells => {
                 cells[0].element(by.css("input")).sendKeys("2018");
                 cells[1].element(by.css("input")).sendKeys("18");
-                cells[2].all(by.css("option")).get(1).click();
+                cells[2].all(by.css("option")).get(0).click();
                 message.clearMessages();
                 e.publish.uploadBtn.click();
                 message.waitForMessage().then(() => {
