28ea38272732d1612d15f0f1a213968b2c9bea31
Nino Junghans <junghans.nino@dainst.de>
2018-10-18 14:18:24 +0200

    Fix e2e Test 'should load some journal data codes...'

--- frontend/test/e2e/specs/main_object.spec.js
+++ frontend/test/e2e/specs/main_object.spec.js
@@ -14,31 +14,28 @@ describe('mainobject view', () => {
                 expect(cell.element(by.css(".alert-warning")).isDisplayed()).toBeTruthy();
             });
         });
-        xit("should load some journal data codes from ojs cilantro plugin api", () => {
+        fit("should load some journal data codes from ojs cilantro plugin api", () => {
             LoginHelper.get(browser, browser.baseUrl);
-            mo.getRowContent("OJS: Journal Code")
-                .then(cell => {
-                    e.home.startBtn.click();
-                    a.clickNavbarButton("articles");
-                    e.subobject.add.click();
-                    so.getRowContent("Language").then(cell => {
-                        // without selected a journal we get default values
-                        expect(cell.all(by.css("label")).get(1).getText()).toBe("English");
-                        expect(cell.all(by.css("label")).get(0).getText()).toBe("German");
-                        expect(cell.all(by.css("label")).count()).toEqual(5);
-                    });
-                    a.restart();
-                    // the journal 0 is selected by default
-                    e.home.startBtn.click();
-                    a.clickNavbarButton("articles");
-                    e.subobject.add.click();
-                    so.getRowContent("Language").then(cell => {
-                        expect(cell.all(by.css("label")).get(1).getText()).toBe("English");
-                        expect(cell.all(by.css("label")).get(0).getText()).toBe("German");
-                        expect(cell.all(by.css("label")).count()).toEqual(5);
-                    });
-
+            e.home.startBtn.click();
+            a.clickNavbarButton("articles");
+            e.subobject.add.click();
+            so.getRowContent("Language").then(cell => {
+                // without selected a journal we get default values
+                expect(cell.all(by.css("label")).get(1).getText()).toBe("English");
+                expect(cell.all(by.css("label")).get(0).getText()).toBe("German");
+            }).then(() => {
+                a.restart();
+                // the journal 0 is selected by default
+                e.home.startBtn.click();
+                a.clickNavbarButton("articles");
+                e.subobject.add.click();
+                so.getRowContent("Language").then(cell => {
+                    expect(cell.all(by.css("label")).get(1).getText()).toBe("English");
+                    expect(cell.all(by.css("label")).get(0).getText()).toBe("German");
                 });
+            });
+
+
 
         });
 
