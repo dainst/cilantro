29f63a21e87bb240b4b8742fb2b936445cbc8e54
Richard Henck <richard.henck@dainst.de>
2018-10-26 18:50:14 +0200

    Fix environment variables for building docu.

--- doc/build-doc.sh
+++ doc/build-doc.sh
@@ -10,6 +10,7 @@ sphinx-apidoc -o . ../
 # environment variables are needed
 BROKER_HOST=broker BROKER_USER=user BROKER_PASSWORD=password \
 DB_HOST=db \
-CONFIG_DIR=/config RESOURCE_DIR=test/resources WORKING_DIR=/data/workspace \
+CONFIG_DIR=../config RESOURCE_DIR=test/resources WORKING_DIR=/data/workspace \
 REPOSITORY_DIR=/data/repository STAGING_DIR=/data/staging \
+OJS_SERVER=ojs OJS_PORT=80 OJS_AUTH_KEY=YWRtaW4=:cGFzc3dvcmQ= \
 make html
