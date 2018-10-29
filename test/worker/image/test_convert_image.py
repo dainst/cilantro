0bf4f1196c8d1a36a05575ff23a7f08e71a2fb99
Nemo Grippa <nemo.grippa@dainst.de>
2018-10-29 13:09:31 +0100

    Cleanup of comments
    - translate a comment
    - remove a comment
    - use consistent spelling of TIF in docstrings

--- test/worker/image/test_convert_image.py
+++ test/worker/image/test_convert_image.py
@@ -21,11 +21,10 @@ class ConvertImageTest(unittest.TestCase):
                     os.path.join(self.resource_dir, 'test_30_40.jpg'), 30, 40)
         self.assertTrue(os.path.isfile(
             f'{self.resource_dir}/test_30_40.jpg'))
-        # os.remove(f'{self.resource_dir}/files/test_30_40.jpg')
         os.remove(os.path.join(self.resource_dir, 'test_30_40.jpg'))
 
     def test_scale_tif(self):
-        """Test scaling for TIFF type image."""
+        """Test scaling for TIF type image."""
         image_file = os.path.join(self.resource_dir, 'files', 'test.tif')
         scale_image(image_file,
                     os.path.join(self.resource_dir, 'test_30_40.tif'), 30, 40)
