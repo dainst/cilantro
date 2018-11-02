de738adba790cfbdcfac9e03463848b309e97fbb
Nemo Grippa <nemo.grippa@dainst.de>
2018-11-02 11:19:34 +0100

    Replace & by &&
    This should start the services in the right order

--- docker/cilantro-frontend-test/test.sh
+++ docker/cilantro-frontend-test/test.sh
@@ -20,13 +20,13 @@ ln -s /salvia/node_modules/webdriver-manager/selenium /usr/share/nginx/html/node
 
 if [ "$TEST" = "mock" ]; then
     echo "Running Frontend tests against mock-backend"
-    npm run e2e-mock-backend & npm run e2e-server & xvfb-run -a -e /dev/stdout -s "-screen 0 2920x2580x24" npm run e2e-test
+    npm run e2e-mock-backend && npm run e2e-server && xvfb-run -a -e /dev/stdout -s "-screen 0 2920x2580x24" npm run e2e-test
 fi
 
 if [ "$TEST" = "default" ]; then
     echo "Running Frontend tests against current docker-compose configuation:"
     cat /salvia/config/settings.json
-    npm run server-nosync & xvfb-run -a -e /dev/stdout -s "-screen 0 2920x2580x24" npm run e2e-test
+    npm run server-nosync && xvfb-run -a -e /dev/stdout -s "-screen 0 2920x2580x24" npm run e2e-test
 fi
 test_res=$?
 echo "TEST Finished. Look for results at http://localhost:7777/test/e2e/screenshots/my-report.html"
