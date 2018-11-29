bf903a8978516d21d33642c5d0503154b252ab6f
Nino Junghans <junghans.nino@dainst.de>
2018-11-29 15:50:11 +0100

    Add user u / p to yml

--- frontend/test/e2e/protractor.conf.js
+++ frontend/test/e2e/protractor.conf.js
@@ -55,7 +55,7 @@ exports.config = {
     onPrepare: () => {
         logFix = {
             specStarted: function(result) {
-                console.log();
+                console.log('');
             }
         };
         jasmine.getEnv().addReporter(logFix);
