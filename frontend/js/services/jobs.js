2da860bc1849279bde2edc8d0b95c8c3ed5f3ac8
Richard Henck <richard.henck@dainst.de>
2018-12-03 10:24:34 +0100

    Remove sub-tasks from the job result page
    The status of these tasks is not clearly defined on the backend and
    can therefore not be reliably queried. Since the information about them
    is also not very useful, they are removed completely. Info about the job
    as a whole is enough here.

--- frontend/js/services/jobs.js
+++ frontend/js/services/jobs.js
@@ -4,7 +4,6 @@ angular
         const jobs = {};
 
         jobs.list = {};
-        jobs.tasks = {};
 
         jobs.updateJob = (jobId, job, isError) => {
 
@@ -12,14 +11,8 @@ angular
                 jobs.list[jobId] = {
                     statusText: 'unknown',
                     status: 0,
-                    task_ids: [],
                     job_id: jobId
                 }
-            }
-
-            // becaus /job/<jobid>- endpoint gives not back tasj-ids for some reason
-            if (!angular.isUndefined(job.task_ids)) {
-                jobs.list[jobId].task_ids = job.task_ids;
             }
 
             if (!isError) {
@@ -29,7 +22,6 @@ angular
                 jobs.list[jobId].statusText = 'Error';
                 jobs.list[jobId].status = -1;
             }
-            jobs.refreshTasks(jobId);
         };
 
         jobs.refresh = () => {
@@ -38,31 +30,6 @@ angular
                 .catch(err => jobs.updateJob(job.job_id, err, true))
             );
         };
-
-        jobs.refreshTasks = jobId => {
-            console.log(jobs.list);
-            Object.values(jobs.list[jobId].task_ids).forEach(taskId => {
-                if (angular.isUndefined(jobs.tasks[jobId])) {
-                    jobs.tasks[taskId] = {
-                        statusText: 'unknown',
-                        status: 0
-                    };
-                }
-                webservice.get('job/' + taskId)
-                    .then(result => {
-                        jobs.tasks[taskId].statusText = result.status;
-                        jobs.tasks[taskId].status = result.status === "SUCCESS" ? 1 : 0;
-                        $rootScope.$broadcast('refreshView');
-                    })
-                    .catch(err => {
-                        jobs.tasks[taskId].statusText = 'error';
-                        jobs.tasks[taskId].status = -1;
-                        $rootScope.$broadcast('refreshView');
-                    });
-            });
-        };
-
-
 
         return jobs;
     }]);
