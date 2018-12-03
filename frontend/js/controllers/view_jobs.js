1d3e16472332f6ec1262548bd9953f9f8de80a28
Richard Henck <richard.henck@dainst.de>
2018-12-03 10:30:51 +0100

    Make the job-status querying stop when all job are done
    Make no sense to continuously query for a status that will never change.

--- frontend/js/controllers/view_jobs.js
+++ frontend/js/controllers/view_jobs.js
@@ -6,7 +6,15 @@ angular
         function($scope, jobs, $interval, steps) {
 
             const refresh = () => {
-                if(steps.current !== 'jobs'){
+                function isJobFinished(job) {
+                    return job[1].status == 1;
+                }
+
+                if (Object.entries(jobs.list).every(isJobFinished)) {
+                    $interval.cancel(promise);
+                }
+
+                if(steps.current !== 'jobs') {
                     $interval.cancel(promise);
                 }
                 else {
