ed8c4e6ba37f8b3b77c4a00d3034e2fdd716c50f
Richard Henck <richard.henck@dainst.de>
2018-12-04 11:49:24 +0100

    Rename docker image build script
    To make it more clear what it actually is for. And free up the name for
    the build script for all of cilantro.

---
+++ docker_image_build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+name=${1}
+tag=${2-latest}
+nocache=${3}
+token=$(grep GITHUB_ACCESS_TOKEN .env | xargs)
+token=${token#*=}
+
+if [ -z "${name}" ]
+    then
+        echo "Please specify a name"
+        read name
+fi
+
+if [ -z "${nocache}" ]
+    then
+        nocache=""
+fi
+
+docker image build -t ${name}:${tag} -f docker/${name}/Dockerfile . --build-arg GITHUB_ACCESS_TOKEN=${token} ${nocache}
+docker tag ${name}:${tag} dainst/${name}:${tag}
+docker push dainst/${name}:${tag}
+exit
