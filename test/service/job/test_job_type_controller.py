7ad4700fcccce33062ff1500f5c347e00f722460
Sebastian Cuy <sebastian.cuy@dainst.de>
2018-12-06 10:36:12 +0100

    Make job type controller tests more robust
    The tests failed when they were run on macOS since os.listdir()
    returned directory entries in a different order and the schemas folder
    appeared first.

--- test/service/job/test_job_type_controller.py
+++ test/service/job/test_job_type_controller.py
@@ -42,7 +42,7 @@ class JobTypeControllerTest(unittest.TestCase):
 
         :return: None
         """
-        first_job_type_name = os.listdir(job_types_dir)[0].rsplit('.', 1)[0]
+        first_job_type_name = _get_first_job_type_file().rsplit('.', 1)[0]
         response_text = self.client.get('/job_types').get_data(as_text=True)
         response_json = json.loads(response_text)
 
@@ -62,18 +62,19 @@ class JobTypeControllerTest(unittest.TestCase):
 
         :return: None
         """
-        first_job_type_file = os.listdir(job_types_dir)[0]
+        first_job_type_file = _get_first_job_type_file()
         with open(os.path.join(job_types_dir, first_job_type_file), 'r') as f:
             first_job_type_file_content = f.read()
         url = '/job_types/' + first_job_type_file.rsplit('.', 1)[0]
         response_text = self.client.get(url).get_data(as_text=True)
 
-        self.assertEqual(yaml.safe_load(first_job_type_file_content), yaml.safe_load(str(response_text)))
+        self.assertEqual(yaml.safe_load(first_job_type_file_content),
+                         yaml.safe_load(str(response_text)))
 
     def test_job_type_detail_with(self):
         """Test if a schema is returned when any job type is requested."""
-        job_type_file = os.listdir(job_types_dir)[0]
-        url = '/job_types/' + job_type_file.rsplit('.', 1)[0] + '?schema=true'
+        first_job_type_file = _get_first_job_type_file()
+        url = '/job_types/' + first_job_type_file.rsplit('.', 1)[0] + '?schema=true'
         response_text = self.client.get(url).get_data(as_text=True)
 
         self.assertTrue("\"schema\":{\"$schema\"" in response_text)
@@ -85,3 +86,7 @@ class JobTypeControllerTest(unittest.TestCase):
         response_json = response.get_json()
         self.assertFalse(response_json['success'])
         self.assertEqual(response_json['error']['code'], "job_type_not_found")
+
+
+def _get_first_job_type_file():
+    return os.path.basename(glob.glob(f"{job_types_dir}/*.yml")[0])
