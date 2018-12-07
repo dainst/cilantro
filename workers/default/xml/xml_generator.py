b298cbe495f299fd6e744fdedd3a0ba2a2de7089
Hannah Blattmann <blattmann.h@gmx.de>
2018-12-07 09:26:05 +0100

    Remove unused variable metadata in METS XML for articles

--- workers/default/xml/xml_generator.py
+++ workers/default/xml/xml_generator.py
@@ -27,6 +27,9 @@ def generate_xml(obj, template_file, target_filepath, additional_params):
     env.globals['datetime'] = datetime.datetime
     env.globals['glob'] = glob.glob
 
+    ojs_metadata = additional_params
+
+
     template = env.get_template(template_file)
     filled_template = template.render(obj=obj,
                                       additional_params=additional_params)
