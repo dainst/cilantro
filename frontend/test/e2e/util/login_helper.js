c7b72e4a61efc22389c48b55f06a5d36c4b4b801
Nino Junghans <junghans.nino@dainst.de>
2018-12-13 11:52:08 +0100

    Add a user setting file for e2e tests

--- frontend/test/e2e/util/login_helper.js
+++ frontend/test/e2e/util/login_helper.js
@@ -1,13 +1,17 @@
 const e = require("../modules/elements");
 const a = require("../modules/actions");
+const users = require("../settings/users")
 const EC = protractor.ExpectedConditions;
 
 const LoginHelper = function () {
-    this.get = (browser, destination, user='test_user', password='test_password') => new Promise((resolve, reject) => {
+    this.get = (browser, destination, userTestId='normal') => new Promise((resolve, reject) => {
         browser.get(destination).then(() => {
             a.clickNavbarButton('loginBtn');
-            e.loginModal.username.clear().sendKeys(user);
-            e.loginModal.password.clear().sendKeys(password);
+
+
+
+            e.loginModal.username.clear().sendKeys(users[userTestId].username);
+            e.loginModal.password.clear().sendKeys(users[userTestId].password);
             e.loginModal.login.click();
             browser.wait(EC.invisibilityOf(e.loginModal.login)).then(resolve);
 
