68831ca06b5a38f9c41f6a2677e7fe5d076c38ca
Sebastian Cuy <sebastian.cuy@dainst.de>
2018-12-13 18:03:01 +0100

    Always refresh jobs
    Will be reimplemented using the new list jobs backend endpoint
    anyway.

--- frontend/js/jobs/jobs.controller.js
+++ frontend/js/jobs/jobs.controller.js
@@ -5,22 +5,7 @@ angular
     .controller('viewJobs',  ['$scope', 'jobs', '$interval', 'steps',
         function($scope, jobs, $interval, steps) {
 
-            const refresh = () => {
-                function isJobFinished(job) {
-                    return job[1].status === 1;
-                }
-
-                if (Object.entries(jobs.list).every(isJobFinished)) {
-                    $interval.cancel(promise);
-                }
-
-                if(steps.current !== 'jobs') {
-                    $interval.cancel(promise);
-                }
-                else {
-                    jobs.refresh();
-                }
-            };
+            const refresh = () => jobs.refresh();
 
             let promise = $interval(refresh, 5000);
 
