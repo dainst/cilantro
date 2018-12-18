8cb325e1cd1e9e265ff51f49227561882773dec1
Richard Henck <richard.henck@dainst.de>
2018-12-18 16:53:53 +0100

    Add mysql library
    This library can be used to communicate with a MySQL database.
    Specifically it is to be used to export books to the Arachne database.

---
+++ utils/mysql.py
@@ -0,0 +1,84 @@
+import os
+import logging
+
+import mysql.connector
+from mysql.connector import Error as MySQLERROR
+
+HOST = os.environ['ARACHNE_DB_HOST']
+DATABASE = os.environ['ARACHNE_DB_NAME']
+USER = os.environ['ARACHNE_DB_USER']
+PASSWORD = os.environ['ARACHNE_DB_PASSWORD']
+
+
+log = logging.getLogger(__name__)
+
+
+def _get_connection():
+    """Establish connection to database."""
+    try:
+        conn = mysql.connector.connect(host=HOST, database=DATABASE,
+                                       user=USER, password=PASSWORD,
+                                       auth_plugin='mysql_native_password')
+        if conn.is_connected():
+            log.debug('Connected to MySQL database')
+        else:
+            log.error('Connection to MySQL failed!')
+    except MySQLERROR as e:
+        log.error(e)
+
+    return conn
+
+
+def insert(query, args):
+    """Commit INSERT query to database."""
+    conn = _get_connection()
+    try:
+        cursor = conn.cursor()
+        cursor.execute(query, args)
+        log.debug(cursor.statement)
+        if cursor.lastrowid:
+            generated_id = cursor.lastrowid
+        else:
+            generated_id = None
+        conn.commit()
+    except MySQLERROR as e:
+        log.error(e)
+    finally:
+        cursor.close()
+        conn.close()
+
+    return generated_id
+
+
+def query(query):
+    """Commit query to database."""
+    conn = _get_connection()
+    try:
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute(query)
+        log.debug(cursor.statement)
+        rows = cursor.fetchall()
+    except MySQLERROR as e:
+        log.error(e)
+    finally:
+        cursor.close()
+        conn.close()
+
+    return rows
+
+
+def delete(query):
+    """Commit DELETE query to database."""
+    conn = _get_connection()
+    try:
+        cursor = conn.cursor()
+        cursor.execute(query)
+        log.debug(cursor.statement)
+        conn.commit()
+        log.debug("All Record Deleted successfully")
+    except MySQLERROR as e:
+        log.error("Failed to Delete all records from database table: {}"
+                  .format(e))
+    finally:
+        cursor.close()
+        conn.close()
