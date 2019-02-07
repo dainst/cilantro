53fb02589f639bde7945d8dd9b82d5afea3e689d
Nino Junghans <junghans.nino@dainst.de>
2019-02-07 16:27:31 +0100

    Remove console logs

--- frontend/js/utils/cookiemanager.js
+++ frontend/js/utils/cookiemanager.js
@@ -5,13 +5,11 @@ angular
         cookiemanager.getCookie = function (name) {
             let re = new RegExp(name + "=([^;]+)");
             let value = re.exec(document.cookie);
-            console.log(value);
             return (value != null) ? JSON.parse(unescape(value[1])) : null;
         };
         cookiemanager.setCookie = function (name, value, days=5) {
             let date = new Date(Date.now()+days*24*60*60*1000);
             document.cookie = name + "=" + JSON.stringify(value)+";"+"expires="+date.toUTCString();
-            console.log(document.cookie);
         };
         cookiemanager.deleteCookie= function (name) {
             document.cookie = name + "=;";
