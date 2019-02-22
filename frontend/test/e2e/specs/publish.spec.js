835ecf8b1fd9a35f1091acb2b118f160eabc1739
Sebastian Cuy <sebastian.cuy@uni-koeln.de>
2019-02-22 17:42:55 +0100

    Fix failing e2e test

--- frontend/test/e2e/specs/publish.spec.js
+++ frontend/test/e2e/specs/publish.spec.js
@@ -11,6 +11,7 @@ describe('publish page', () => {
 
     fit("should not allow start job if no articles are staged", () => {
         so.goToSubObject(2);
+        browser.wait(EC.presenceOf(e.navbar.publish), 20000);
         expect(e.navbar.publish.isDisplayed()).toBeFalsy();
         browser.wait(EC.visibilityOf(e.subobject.confirmBtn), 20000);
         e.subobject.confirmBtn.click();
