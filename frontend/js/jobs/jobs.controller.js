66b74683fb47cb85569780f463bb407f7a83fc77
Sebastian Cuy <sebastian.cuy@dainst.de>
2019-03-06 10:36:35 +0100

    Cancel timer when leaving jobs list
    Closes #10507

--- frontend/js/jobs/jobs.controller.js
+++ frontend/js/jobs/jobs.controller.js
@@ -1,15 +1,18 @@
 angular
 
-    .module('controller.viewJobs', [])
+    .module("controller.viewJobs", [])
 
-    .controller('viewJobs',  ['$scope', 'jobs', '$interval', 'steps',
+    .controller("viewJobs",  ["$scope", "jobs", "$interval", "steps",
         function($scope, jobs, $interval, steps) {
 
             const refresh = () => jobs.refresh();
 
+            refresh();
             let promise = $interval(refresh, 5000);
 
             $scope.jobs = jobs.list;
+
+            $scope.$on("$destroy", () => $interval.cancel(promise));
 
         }
     ]);
