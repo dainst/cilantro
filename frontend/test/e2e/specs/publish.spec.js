954c669c404d54d648c0e8d08b944e13b408e91b
Sebastian Cuy <sebastian.cuy@dainst.de>
2019-03-08 12:10:55 +0100

    Remove fit

--- frontend/test/e2e/specs/publish.spec.js
+++ frontend/test/e2e/specs/publish.spec.js
@@ -9,7 +9,7 @@ const EC = protractor.ExpectedConditions;
 
 describe('publish page', () => {
 
-    fit("should not allow start job if no articles are staged", () => {
+    it("should not allow start job if no articles are staged", () => {
         so.goToSubObject(2);
         expect(e.navbar.publish.isPresent()).toBeFalsy();
         browser.wait(EC.visibilityOf(e.subobject.confirmBtn), 20000);
@@ -17,7 +17,7 @@ describe('publish page', () => {
         expect(e.navbar.publish.isDisplayed()).toBeTruthy();
     });
 
-    fit("should not allow start job if mainobject data is no valid", () => {
+    it("should not allow start job if mainobject data is no valid", () => {
         so.goToSubObject(2);
         browser.wait(EC.visibilityOf(e.subobject.confirmBtn), 5000);
         e.subobject.confirmBtn.click();
