f372409544f8cc4552735c59e1317ea9e4a883ea
Sebastian Cuy <sebastian.cuy@dainst.de>
2019-03-08 11:57:17 +0100

    Hard-code wizard navigation
    This makes sure only past steps are accessible without having to rely
    on a complex routing as it was implemented in steps.js.

--- frontend/js/jobs/wizard/wizard.controller.js
+++ frontend/js/jobs/wizard/wizard.controller.js
@@ -3,14 +3,6 @@ angular.module("workbench.jobs.wizard")
     .controller("createController", ["$scope", "$location", "dataset", "webservice",
         function ($scope, $location, dataset, webservice) {
 
-            $scope.steps = {
-                "start": "Start",
-                "documents": "Documents",
-                "overview": "Overview",
-                "subobjects": "Articles",
-                "finish": "Publish"
-            }
-
             webservice.get(["ojs_url", 'journalInfo'])
                 .then((journalInfo) => dataset.setConstraints(journalInfo.data));
 
