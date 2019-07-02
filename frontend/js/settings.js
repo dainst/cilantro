20c3f1b542ee879d4ffc36181becdcbd7cff66c2
Richard Henck <richard.henck@dainst.de>
2019-07-02 17:46:15 +0200

    Fix code style and remove log statement

--- frontend/js/settings.js
+++ frontend/js/settings.js
@@ -1,26 +1,25 @@
 angular.module('workbench')
 
-	.factory("settings", ['$http', function($http) {
+    .factory("settings", ['$http', function($http) {
 
-		let loaded = false;
-		let settings = {};
+        let loaded = false;
+        let settings = {};
 
-	    settings.load = () => {
-	        return $http.get('config/settings.json').then(response => {
+        settings.load = () => {
+            return $http.get('config/settings.json').then(response => {
                 settings = angular.extend(settings, response.data);
-				return $http.get('version.json');
-			}).then(response => {
-				settings.versionInfo = response.data;
-				console.log('settings', settings);
-				return settings;
-			});
-	    }
+                return $http.get('version.json');
+            }).then(response => {
+                settings.versionInfo = response.data;
+                return settings;
+            });
+        }
 
-		settings.get = () => {
-			if (loaded) return Promise.resolve(settings);
-			else return settings.load();
-		}
+        settings.get = () => {
+            if (loaded) return Promise.resolve(settings);
+            else return settings.load();
+        }
 
-		return settings;
+        return settings;
 
-	}]);
+    }]);
