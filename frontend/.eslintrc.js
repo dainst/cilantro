ec5a63652b60ab9f6590911b2dd14dc7f01dd76a
Nemo Grippa <nemo.grippa@dainst.de>
2019-07-18 13:05:07 +0200

    Remove unnecessary tab rule from eslint config

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -11,23 +11,22 @@ module.exports = {
     rules: {
         'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        "indent": ["error", "tab"],
-        "indent": ["error", 4],
-        "space-before-function-paren": ["error", "never"],
-        "vue/script-indent": ["error", 4, {
-            "baseIndent": 1,
-            "switchCase": 0,
-            "ignores": []
+        'indent': ['error', 4],
+        'space-before-function-paren': ['error', 'never'],
+        'vue/script-indent': ['error', 4, {
+            'baseIndent': 1,
+            'switchCase': 0,
+            'ignores': []
         }]
     },
-    "overrides": {
-        "files": ["*.vue"],
-        "rules": {
-            "indent": "off"
+    'overrides': {
+        'files': ['*.vue'],
+        'rules': {
+            'indent': 'off'
         }
     },
     parserOptions: {
         parser: '@typescript-eslint/parser'
     },
-    "plugins": ["html"]
+    'plugins': ['html']
 }
