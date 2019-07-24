e62b9a9ab130e348930ad58697cd64de2587a2a0
Richard Henck <richard.henck@dainst.de>
2019-07-24 15:04:39 +0200

    Add lint rule to ignore param reassignment in store mutations
    The way vuex handles the mutation method signtures violates
    the airbnb rules. This make the linter ignore that.

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -13,7 +13,15 @@ module.exports = {
         'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         'indent': ['error', 4],
-        'space-before-function-paren': ['error', 'never']
+        'space-before-function-paren': ['error', 'never'],
+        'no-param-reassign': [
+            'error',
+            {
+                props: true, ignorePropertyModificationsFor: [
+                    'state', // for vuex state
+                ]
+            }
+        ]
     },
     parserOptions: {
         parser: '@typescript-eslint/parser'
