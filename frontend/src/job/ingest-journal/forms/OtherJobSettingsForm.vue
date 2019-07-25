74703ff106a9a077662fd2f3501d5c81e68189a1
Richard Henck <richard.henck@dainst.de>
2019-07-25 18:42:21 +0200

    Add and improve forms for journal import

---
+++ frontend/src/job/ingest-journal/forms/OtherJobSettingsForm.vue
@@ -0,0 +1,30 @@
+<template>
+    <section>
+        <b-field grouped group-multiline>
+            <b-field label="nlp_language">
+                <b-input v-model="settings.nlp_language"></b-input>
+            </b-field>
+            <b-field label="do_ocr">
+                <b-switch v-model="settings.do_ocr"></b-switch>
+            </b-field>
+            <b-field label="keep_ratio">
+                <b-switch v-model="settings.keep_ratio"></b-switch>
+            </b-field>
+        </b-field>
+    </section>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+@Component
+export default class ArticleMetadataForm extends Vue {
+    @Prop() private initialData!: Object
+
+    data() {
+        return {
+            settings: this.$props.initialData
+        };
+    }
+}
+</script>
