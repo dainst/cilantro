9a473b0345df8998a36ae378a6aabedc44b8a33b
Simon Hohl <simon.hohl@dainst.org>
2019-07-30 18:45:34 +0200

    Lint all the things!

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -24,62 +24,57 @@ export default new Vuex.Store({
     },
     mutations: {
         auth_prompt: (state) => {
-            state.authentication.status = AuthenticationStatus.Prompt
+            state.authentication.status = AuthenticationStatus.Prompt;
         },
         auth_request: (state) => {
-            state.authentication.status = AuthenticationStatus.Pending
+            state.authentication.status = AuthenticationStatus.Pending;
         },
         auth_success: (state, [user, password]) => {
-            state.authentication.status = AuthenticationStatus.In
-            state.authentication.credentials.name = user
-            state.authentication.credentials.password = password
+            state.authentication.status = AuthenticationStatus.In;
+            state.authentication.credentials.name = user;
+            state.authentication.credentials.password = password;
         },
         auth_error: (state) => {
-            state.authentication.status = AuthenticationStatus.Error
+            state.authentication.status = AuthenticationStatus.Error;
         },
 
         logout: (state) => {
-            state.authentication.status = AuthenticationStatus.Out
-        },
+            state.authentication.status = AuthenticationStatus.Out;
+        }
     },
     actions: {
-        login: ({ commit }, user) => {
-            return new Promise((resolve, reject) => {
-                commit('auth_request')
-                axios({
-                    url: 'http://localhost:5000/user/' + user.name,
-                    auth: { username: user.name, password: user.password }, method: 'GET'
+        login: ({ commit }, user) => new Promise((resolve, reject) => {
+            commit('auth_request');
+            axios({
+                url: `http://localhost:5000/user/${user.name}`,
+                auth: { username: user.name, password: user.password },
+                method: 'GET'
+            })
+                .then((resp) => {
+                    localStorage.setItem('username', user.name);
+                    localStorage.setItem('password', user.password);
+                    axios.defaults.headers.common.Authorization = `Basic ${btoa(`${user.name}:${user.password}`)}`;
+                    commit('auth_success', user.name, user.password);
+                    resolve(resp);
                 })
-                    .then(resp => {
-                        localStorage.setItem('username', user.name)
-                        localStorage.setItem('password', user.password)
-                        axios.defaults.headers.common['Authorization'] = 'Basic ' + btoa(user.name + ':' + user.password);
-                        commit('auth_success', user.name, user.password)
-                        resolve(resp)
-                    })
-                    .catch(err => {
-                        commit('auth_error')
-                        localStorage.removeItem('username')
-                        localStorage.removeItem('password')
-                        reject(err)
-                    })
-            })
-        },
-        logout: ({ commit }) => {
-            return new Promise((resolve, reject) => {
-                commit('logout')
-                localStorage.removeItem('username')
-                localStorage.removeItem('password')
-                delete axios.defaults.headers.common['Authorization']
-                resolve()
-            })
-        },
-        promptLogin: ({ commit }) => {
-            return new Promise((resolve, reject) => {
-                commit('auth_prompt')
-                resolve()
-            })
-        }
+                .catch((err) => {
+                    commit('auth_error');
+                    localStorage.removeItem('username');
+                    localStorage.removeItem('password');
+                    reject(err);
+                });
+        }),
+        logout: ({ commit }) => new Promise((resolve, reject) => {
+            commit('logout');
+            localStorage.removeItem('username');
+            localStorage.removeItem('password');
+            delete axios.defaults.headers.common.Authorization;
+            resolve();
+        }),
+        promptLogin: ({ commit }) => new Promise((resolve, reject) => {
+            commit('auth_prompt');
+            resolve();
+        })
     },
     getters: {
         isAuthenticated: state => (state.authentication.status === AuthenticationStatus.In),
