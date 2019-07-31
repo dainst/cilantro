4d58d950cdadd8da69b70f086c847af32bf5b7c2
Simon Hohl <simon.hohl@dainst.org>
2019-07-31 12:35:44 +0200

    Fix "Assignment Branch Condition too high"  error
    See also: https://hub.codebeat.co/docs/software-quality-metrics#assignment-branch-condition

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -53,8 +53,9 @@ export default new Vuex.Store({
                 });
                 localStorage.setItem('username', user.name);
                 localStorage.setItem('password', user.password);
-                axios.defaults.headers.common.Authorization =
-                    `Basic ${btoa(`${user.name}:${user.password}`)}`;
+
+                const basicAuth = `Basic ${btoa(`${user.name}:${user.password}`)}`;
+                axios.defaults.headers.common.Authorization = basicAuth;
                 commit('auth_success', user.name, user.password);
                 resolve(response);
             } catch (err) {
