c4b629fc36a69e6a7ca5362b0f18c665e1b495e0
Sebastian Cuy <sebastian.cuy@dainst.de>
2019-08-01 11:01:44 +0200

    Create separate module for authentication

--- frontend/src/dashboard/MainMenu.vue
+++ frontend/src/dashboard/MainMenu.vue
@@ -1,17 +1,17 @@
 <template>
-    <div>
-        <Authentication />
-    </div>
+  <div>
+    <Authentication />
+  </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import Authentication from './Authentication.vue';
+import { Component, Vue } from "vue-property-decorator";
+import Authentication from "../authentication/Authentication.vue";
 
 @Component({
-    components: {
-        Authentication
-    }
+  components: {
+    Authentication
+  }
 })
 export default class MainMenu extends Vue {}
 </script>
