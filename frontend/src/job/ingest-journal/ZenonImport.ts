faf2b2be5148ea3bb38a727f3f70a4269e8786ac
Richard Henck <richard.henck@dainst.de>
2019-08-09 12:00:11 +0200

    Add Zenon Search for ID using the record-endpoint
    This way search can give the exact record on is looking for withou 
    having to browse a list of results when using another search term.

--- frontend/src/job/ingest-journal/ZenonImport.ts
+++ frontend/src/job/ingest-journal/ZenonImport.ts
@@ -1,23 +1,38 @@
 import axios from 'axios';
 
-const zenonSearchUri = 'https://zenon.dainst.org/api/v1/search';
+const zenonBaseURL: string = 'https://zenon.dainst.org/api/v1/';
 
-    const params = {
-        lookfor: term
-    };
+function isId(term: string) {
+    return !!term.match(/^\w?\w?\d{9}$/);
+}
+
 export default async function search(term: string): Promise<ZenonResultData> {
+    let apiEndpoint: string = '';
+    let params: object = {};
+    if (!isId(term)) {
+        apiEndpoint = 'search';
+        params = {
+            lookfor: term,
+            type: 'title'
+        };
+    } else {
+        apiEndpoint = 'record';
+        params = {
+            id: term
+        };
+    }
 
     try {
-        const response = await axios.get(
-            zenonSearchUri, {
-                params,
-                transformRequest: [(data, headers) => {
-                    delete headers.common.Authorization; // eslint-disable-line no-param-reassign
-                    return data;
-                }]
-            }
-        );
-        return response.data.records;
+        const response = await axios({
+            method: 'get',
+            url: `${zenonBaseURL}${apiEndpoint}`,
+            params,
+            transformRequest: [(data, headers) => {
+                delete headers.common.Authorization; // eslint-disable-line no-param-reassign
+                return data;
+            }]
+        });
+        return response.data;
     } catch (err) {
         console.error(err);
         return {
