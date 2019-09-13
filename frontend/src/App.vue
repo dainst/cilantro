e5bb06ccd68c8baf5d575505b7e867d91e63d0f5
Sebastian Cuy <sebastian.cuy@dainst.de>
2019-09-13 15:10:08 +0200

    Fix code style

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,21 +7,19 @@
 
         <Navbar></Navbar>
 
-        <router-view id="main_content"/>
-        
+        <router-view id="main_content" />
+
         <footer class="footer">
             <div class="content has-text-centered">
                 <p>
-                  v{{version}}
-                  <small>(
-                      <b-tooltip :label="githash">
-                        {{githash.substr(0, 7)}}
-                      </b-tooltip>
-                  )</small>
+                    v{{version}}
+                    <small>
+                        (
+                        <b-tooltip :label="githash">{{githash.substr(0, 7)}}</b-tooltip>)
+                    </small>
                 </p>
             </div>
         </footer>
-        
     </div>
 </template>
 
@@ -36,46 +34,44 @@ import Navbar from './Navbar.vue';
     }
 })
 export default class App extends Vue {
-
     version: string = process.env.VUE_APP_VERSION;
     githash: string = process.env.VUE_APP_GITHASH;
-
 }
 </script>
 
 <style lang="scss">
-    #app {
+#app {
     font-family: "Avenir", Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+
+    position: relative;
+    min-height: 100vh;
+}
+
+#nav {
+    padding: 30px;
+
+    a {
+        font-weight: bold;
         color: #2c3e50;
 
-        position: relative;
-        min-height: 100vh;
-    }
-
-    #nav {
-        padding: 30px;
-
-        a {
-            font-weight: bold;
-            color: #2c3e50;
-
-            &.router-link-exact-active {
-                color: #42b983;
-            }
+        &.router-link-exact-active {
+            color: #42b983;
         }
     }
+}
 
-    #main_content {
-      padding-bottom: 10rem;  /* Footer height */
-    }
+#main_content {
+    padding-bottom: 10rem; /* Footer height */
+}
 
-    footer {
-      position: absolute;
-      bottom: 0;
-      width: 100%;
-      height: 0.5rem;     /* Footer height */
-    }
+footer {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 0.5rem; /* Footer height */
+}
 </style>
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,5 @@
-process.env.VUE_APP_VERSION = require('./package.json').version
-process.env.VUE_APP_GITHASH = require('child_process').execSync('git rev-parse HEAD')
+process.env.VUE_APP_VERSION = require('./package.json').version;
+process.env.VUE_APP_GITHASH = require('child_process').execSync('git rev-parse HEAD');
 
 module.exports = {
     lintOnSave: false
