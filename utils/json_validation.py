65db6d7bb9436b4dc888445ebda426cd85edc9a1
Richard Henck <richard.henck@dainst.de>
2019-09-16 13:28:09 +0200

    Update utility class for moved schema files

--- utils/json_validation.py
+++ utils/json_validation.py
@@ -7,11 +7,10 @@ def validate_params(params, job_type):
     """
     Validate JSON string against a schema file.
 
-    The schema file is taken from a subdirectory of the job-type definitions
-    with the appendix '_schema.json'.
+    The schema file is taken from a config subdirectory.
     """
     resource_dir = os.environ['CONFIG_DIR']
-    schema_path = os.path.join(resource_dir, 'job_types', 'schemas',
+    schema_path = os.path.join(resource_dir, 'job_parameter_schemas',
                                job_type + '_schema.json')
     with open(schema_path, 'r') as schema_file:
         job_type_schema = json.loads(schema_file.read())
