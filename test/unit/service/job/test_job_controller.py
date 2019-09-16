d3c93324e58caceaaaf59f8f3479839b97439858
Richard Henck <richard.henck@dainst.de>
2019-09-16 18:15:41 +0200

    Remove tests for yaml job configs

--- test/unit/service/job/test_job_controller.py
+++ test/unit/service/job/test_job_controller.py
@@ -5,8 +5,6 @@ import datetime
 
 from pymongo import MongoClient
 
-from unittest.mock import patch
-
 from service.run_service import app
 from test.unit.service.user.user_utils import get_auth_header
 
@@ -14,16 +12,17 @@ from test.unit.service.user.user_utils import get_auth_header
 class JobControllerTest(unittest.TestCase):
     """Test the job controller to validate the parameter payload."""
 
+    test_resource_dir = 'test/resources'
+
     def setUp(self):
         app.testing = True
         self.client = app.test_client()
-        self.env = patch.dict(
-            'os.environ',
-            {'CONFIG_DIR': "test/resources/configs/config_valid"})
-        self.env.start()
 
-    def tearDown(self):
-        self.env.stop()
+    def test_get_job_type_schema(self):
+        """Test if a ingest-journal schema is returned when requested."""
+        url = '/job/ingest_journal'
+        response_text = self.client.get(url).get_data(as_text=True)
+        self.assertTrue("{\"$schema\"" in response_text)
 
     def test_list_job_filter(self):
         """
@@ -35,22 +34,14 @@ class JobControllerTest(unittest.TestCase):
         for appearance of the jobs. Jobs older than the threshold and being
         successful are not to show up.
         """
-        data = {
-            "metadata": {
-                "title": "Test-Title",
-                "description": "Test-Description",
-                "year": 1992
-                },
-            "files": [
-                {"file": "some_tiffs/test.tif"},
-                {"file": "some_tiffs/test2.tiff"}
-                ]
-            }
-        job1_response = self._make_request('/job/job2', json.dumps(data), 202)
-        job_id1 = job1_response.get_json()['job_id']
-        job2_response = self._make_request('/job/job2', json.dumps(data), 202)
-        job_id2 = job2_response.get_json()['job_id']
-        self._make_request('/job/job2', json.dumps(data), 202)
+        job_params = self._read_test_params()
+        job1_response = self._make_request('/job/ingest_journal',
+                                           json.dumps(job_params), 202)
+        job_id1 = job1_response.get_json()['job_ids'][0]
+        job2_response = self._make_request('/job/ingest_journal',
+                                           json.dumps(job_params), 202)
+        job_id2 = job2_response.get_json()['job_ids'][0]
+        self._make_request('/job/ingest_journal', json.dumps(job_params), 202)
 
         # hack to change the updated timestamp and job status on some test jobs
         client = MongoClient(os.environ['JOB_DB_URL'],
@@ -77,7 +68,7 @@ class JobControllerTest(unittest.TestCase):
         self.assertTrue(len((job_list_all_json)) >
                         len((job_list_filtered_json)))
 
-    def test_create_job_success_and_list_job(self):
+    def test_create_ingest_journal_job(self):
         """
         Test listing of jobs.
 
@@ -85,132 +76,57 @@ class JobControllerTest(unittest.TestCase):
         The job list is checked for existence of a job-id, the test user,
         the correct job type and job name.
         """
-        data = {
-            "metadata": {
-                "title": "Test-Title",
-                "description": "Test-Description",
-                "year": 1992
-                },
-            "files": [
-                {"file": "some_tiffs/test.tif"},
-                {"file": "some_tiffs/test2.tiff"}
-                ],
-            "parts": [{
-                "files": [
-                    {"file": "test.pdf"}
-                    ]
-                }]
-            }
-        self._make_request('/job/job2', json.dumps(data), 202)
+        job_params = self._read_test_params()
+        self._make_request('/job/ingest_journal',
+                           json.dumps(job_params), 202)
+
+    def test_list_jobs(self):
+        """
+        Test creating and listing jobs.
+
+        It is tested if some parameters show up in the job list response.
+        """
+        self.test_create_ingest_journal_job()
 
         response = self.client.get('/job/jobs', headers=get_auth_header())
         last_job_json = response.get_json()[-1]
 
         self.assertTrue(last_job_json["job_id"])
         self.assertEqual("test_user", last_job_json["user"])
-        self.assertEqual("job2", last_job_json["job_type"])
-        self.assertEqual('JOB-job2-some_tiffs/test.tif', last_job_json["name"])
-
-    def test_create_job_success_and_list_job_without_files(self):
-        """
-        Test listing of jobs with empty files-object.
-
-        The test creates a job and then gets a list of all jobs.
-        The job list is checked for existence of a job-id, the test user,
-        the correct job type and job name.
-        """
-        data = {
-            "metadata": {
-                "title": "Test-Title",
-                "description": "Test-Description",
-                "year": 1992
-                },
-            "files": [],
-            "parts": [{
-                "files": [
-                    {"file": "test.pdf"}
-                    ]
-                }]
-            }
-        self._make_request('/job/job2', json.dumps(data), 202)
-
-        response = self.client.get('/job/jobs', headers=get_auth_header())
-        last_job_json = response.get_json()[-1]
-
-        self.assertTrue(last_job_json["job_id"])
-        self.assertEqual("test_user", last_job_json["user"])
-        self.assertEqual("job2", last_job_json["job_type"])
-        self.assertEqual('JOB-job2-test.pdf', last_job_json["name"])
+        self.assertEqual("ingest_journal", last_job_json["job_type"])
+        self.assertEqual('JOB-ingest_journal-test-folder1',
+                         last_job_json["name"])
 
     def test_create_job_no_payload(self):
         """Job creation has to fail without POST payload."""
-        self._make_request('/job/job1', None, 400, 'invalid_job_params',
-                           'No request payload found')
+        self._make_request('/job/ingest_journal', None, 400,
+                           'invalid_job_params', 'No request payload found')
 
     def test_create_job_invalid_json(self):
         """Test job creation to fail when the POST payload is not JSON."""
-        self._make_request('/job/job1', 'jkhsadfj,;', 400, 'bad_request')
-
-    def test_create_job_unknown_job_type(self):
-        """Test job creation to fail when the job definition is not found."""
-        self._make_request('/job/job3', '{}', 404, 'unknown_job_type',
-                           'No job definition file found')
-
-    def test_create_job_no_schema(self):
-        """Test failing job creation when no schema file is found."""
-        error_message = ("[Errno 2] No such file or directory: "
-                         "'test/resources/configs/config_valid/job_types"
-                         "/schemas/job1_schema.json'")
-        self._make_request('/job/job1', '{}', 404, 'unknown_job_type',
-                           error_message)
+        self._make_request('/job/ingest_journal', 'jkfj,;', 400, 'bad_request')
 
     def test_create_job_unknown_param(self):
         """Test job creation to fail when there are unknown params given."""
-        data = {
-            "metadata": {
-                "title": "Test-Title",
-                "description": "Test-Description",
-                "year": 1992
-            },
-            "files": [
-                {"file": "some_tiffs/test.tif"},
-                {"file": "some_tiffs/test2.tiff"}
-            ],
-            "bla": "blub"
-        }
-        self._make_request('/job/job2', json.dumps(data), 400,
+        job_params = self._read_test_params()
+        job_params['bla'] = 'blup'
+
+        self._make_request('/job/ingest_journal', json.dumps(job_params), 400,
                            'invalid_job_params',
                            'Additional properties are not allowed')
 
     def test_create_job_missing_param(self):
         """Test job creation to fail when there are params missing."""
-        data = {
-            "metadata": {
-                "title": "Test-Title",
-                "year": 1992
-            },
-            "files": [
-                {"file": "some_tiffs/test.tif"},
-                {"file": "some_tiffs/test2.tiff"}
-            ]
-        }
-        self._make_request('/job/job2', json.dumps(data), 400,
+        job_params = self._read_test_params()
+        del job_params['objects'][0]['metadata']['volume']
+        self._make_request('/job/ingest_journal', json.dumps(job_params), 400,
                            'invalid_job_params', 'is a required property')
 
     def test_create_job_wrong_param_type(self):
         """Test job creation to fail when a param has the wrong type."""
-        data = {
-            "metadata": {
-                "title": "Test-Title",
-                "description": "Test-Description",
-                "year": "1992"
-            },
-            "files": [
-                {"file": "some_tiffs/test.tif"},
-                {"file": "some_tiffs/test2.tiff"}
-            ]
-        }
-        self._make_request('/job/job2', json.dumps(data), 400,
+        job_params = self._read_test_params()
+        job_params['objects'][0]['metadata']['volume'] = 5453
+        self._make_request('/job/ingest_journal', json.dumps(job_params), 400,
                            'invalid_job_params', 'is not of type')
 
     def _make_request(self, job_name, payload, expected_http_code,
@@ -231,3 +147,12 @@ class JobControllerTest(unittest.TestCase):
                             response_json['error']['message'])
 
         return response
+
+    def _read_test_params(self):
+        test_params_path = os.path.join(self.test_resource_dir, 'params',
+                                        'journal.json')
+
+        with open(test_params_path, 'r') as params_file:
+            job_params = json.loads(params_file.read())
+
+        return job_params
