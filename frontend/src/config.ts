a1cc5b58e28e3dd2a305718def5972dd19f33a7d
Sebastian Cuy <sebastian.cuy@dainst.de>
2019-09-19 16:49:10 +0200

    Move ojsZenonMapping to separate config file

---
+++ frontend/src/config.ts
@@ -0,0 +1,11 @@
+// Mapping von Zenon-IDs zu OJS Journal Codes
+export const ojsZenonMapping = {
+    '000054957': 'aa',
+    '000055021': 'brgk',
+    '000098143': 'chiron',
+    '001376930': 'efb', // TODO: kein übergeordneter Datensatz in Zenon
+    '001376922': 'ejb', // TODO: kein übergeordneter Datensatz in Zenon
+    '000054792': 'ger',
+    '000055658': 'mm',
+    '000814258': 'rm'
+} as { [index: string]: string };
