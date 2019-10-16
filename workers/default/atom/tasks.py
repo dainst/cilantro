d9de9642979b282da7f63ce3bb79820b7569d2ab
Sebastian Cuy <sebastian.cuy@dainst.de>
2019-10-16 16:04:09 +0200

    Implement publish_to_atom task

---
+++ workers/default/atom/tasks.py
@@ -0,0 +1,28 @@
+import os
+
+from utils.celery_client import celery_app
+
+from workers.base_task import ObjectTask
+from workers.default.atom.atom_api import create_digital_object
+
+
+def _generate_ojs_id(prefix, journal_code, result_id):
+    return f"{prefix}-{journal_code}-{result_id}"
+
+
+class PublishToAtomTask(ObjectTask):
+    """
+    Add a digital object for the PDF in the repository to AtoM.
+
+    Preconditions:
+    - object metadata must contain atom_id
+    - a PDF file named after the object id has to be present in data/pdf/
+    """
+
+    name = "publish_to_atom"
+
+    def process_object(self, obj):
+        create_digital_object(obj)
+
+
+PublishToAtomTask = celery_app.register_task(PublishToAtomTask())
