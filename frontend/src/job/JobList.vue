f7e85d7b4b39390c3028449e94fcb0c6e6e67347
Nino Junghans <junghans.nino@dainst.de>
2019-10-17 15:29:19 +0200

    Fix Date sorting in joblist

--- frontend/src/job/JobList.vue
+++ frontend/src/job/JobList.vue
@@ -2,7 +2,7 @@
     <section>
         <div v-if="jobList.length > 0">
         <b-table :data="jobList" detailed detail-key="job_id"
-            default-sort="created" :default-sort-direction="'desc'">
+            default-sort="created" :default-sort-direction="'asc'">
             <template slot-scope="props">
                 <b-table-column field="job_id" label="ID" sortable>
                     {{ props.row.job_id }}
@@ -15,10 +15,10 @@
                 <b-table-column field="state" label="Status" sortable>
                     {{ props.row.state }}
                 </b-table-column>
-                <b-table-column field="created" label="Created" sortable>
+                <b-table-column field="created" label="Created" :custom-sort="sortByCreated" sortable>
                     {{ props.row.created }}
                 </b-table-column>
-                <b-table-column field="updated" label="Updated" sortable>
+                <b-table-column field="updated" label="Updated" :custom-sort="sortByUpdated" sortable>
                     {{ props.row.updated }}
                 </b-table-column>
                 <b-table-column>
@@ -46,6 +46,7 @@ import { Component, Vue } from 'vue-property-decorator';
 import { getJobList } from './JobClient';
 import { Job } from './Job';
 import { showError } from '@/util/Notifier.ts';
+import moment from 'moment'
 
 @Component
 export default class JobList extends Vue {
@@ -62,9 +63,28 @@ export default class JobList extends Vue {
         });
     }
 
+    sortByUpdated(a:Job, b:Job, isAsc:boolean) {
+        var d1 = moment(a.updated, 'ddd, DD MMM YYYY HH:mm:ss').toDate();
+        var d2 = moment(b.updated, 'ddd, DD MMM YYYY HH:mm:ss').toDate();
+        return this.compareDates(d1,d2,isAsc);
+    }
+    sortByCreated(a:Job, b:Job, isAsc:boolean) {
+        var d1 = moment(a.created, 'ddd, DD MMM YYYY HH:mm:ss').toDate();
+        var d2 = moment(b.created, 'ddd, DD MMM YYYY HH:mm:ss').toDate();
+        return this.compareDates(d1,d2,isAsc);
+    }
+    compareDates(a:Date, b:Date, isAsc:boolean){
+        if (isAsc) {
+            return b.getTime()-a.getTime();
+        } else {
+            return a.getTime() - b.getTime();
+        }
+    }
+
     async updateJobList() {
         try {
             this.jobList = await getJobList();
+            console.log(this.jobList)
         } catch (e) {
             showError('Failed to load job list from server!', e);
         }
