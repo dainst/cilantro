50001c4ac66e1a2438a6adb247b180f2e03a8ab3
Richard Henck <richard.henck@dainst.de>
2019-11-07 14:12:40 +0100

    Raise mal length for folder name from 20 to 30 characters

--- frontend/src/staging/StagingBrowser.vue
+++ frontend/src/staging/StagingBrowser.vue
@@ -85,7 +85,7 @@ export default class StagingBrowser extends Vue {
         this.$buefy.dialog.prompt({
             message: `Enter the folder name or path`,
             inputAttrs: {
-                maxlength: 20
+                maxlength: 30
             },
             onConfirm: (folderName) => {
                 createFolderInStaging(getFilePath(this.workingDirectory, folderName))
