f577f975abbd3f6aba7f7525a9e928c09e96b1d5
Sebastian Cuy <sebastian.cuy@dainst.de>
2019-11-26 14:35:01 +0100

    Set ATOM_API_KEY from secret in service entrypoint

--- docker/cilantro-service/entrypoint.sh
+++ docker/cilantro-service/entrypoint.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+
+if test -f "/run/secrets/atom_api_key"; then
+    echo "Found secret: setting ATOM_API_KEY"
+    export ATOM_API_KEY=$(cat "/run/secrets/atom_api_key")
+fi
+
 if [ "$CILANTRO_ENV" = "development" ]
 then
     watchmedo auto-restart -R -d service -d config -p="*.py;*.yml" -- flask run --host=0.0.0.0
