6c71983ec9a18d8aab283af1eff2acd453db3528
Sebastian Cuy <sebastian.cuy@dainst.de>
2019-11-27 12:50:44 +0100

    Use gevent workers in production mode
    This hopefully fixes WORKER TIMEOUT errors in gunicorn when uploading
    large files.
    
    see #11542

--- docker/cilantro-service/entrypoint.sh
+++ docker/cilantro-service/entrypoint.sh
@@ -9,5 +9,5 @@ if [ "$CILANTRO_ENV" = "development" ]
 then
     watchmedo auto-restart -R -d service -d config -p="*.py;*.yml" -- flask run --host=0.0.0.0
 else
-    gunicorn -w 4 -b :5000 service.run_service:app
+    gunicorn -w 4 -k gevent -b :5000 service.run_service:app
 fi
