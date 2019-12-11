2840db6cbd7dc0748ea616fe38bbc01c373060f5
Sebastian Cuy <sebastian.cuy@dainst.de>
2019-12-11 17:42:18 +0100

    Some UI changes

--- frontend/src/dashboard/StartJobSelection.vue
+++ frontend/src/dashboard/StartJobSelection.vue
@@ -4,18 +4,26 @@
         <div class="tile is-ancestor">
             <div class="tile is-parent">
                 <div class="tile is-child box">
-                    <p class="title">Journal import</p>
-                    <p>Job Type Description</p>
+                    <p class="title">Retrodigitized Journals</p>
+                    <p>
+                        Import multiple folders that contain scans of journal issues
+                        into iDAI.publications / OJS.
+                    </p>
                     <router-link to="ingest-journal" tag="b-button" id="start_ingest_journal">Start</router-link>
                 </div>
                 <div class="tile is-child box">
-                    <p class="title">Record import</p>
-                    <p>Job Type Description</p>
+                    <p class="title">Retrodigitized Archival Records</p>
+                    <p>
+                        Import multiple folders that contain scans of archival material
+                        into iDAI.archives / AtoM.
+                    </p>
                     <router-link to="ingest-record" tag="b-button" id="start_ingest_record">Start</router-link>
                 </div>
                 <div class="tile is-child box">
                     <p class="title">NLP Processing</p>
-                    <p>Job Type Description</p>
+                    <p>
+                        <i>work in progress ...</i>
+                    </p>
                 </div>
             </div>
         </div>
