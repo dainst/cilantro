b037110f62465b67af9aee9a9a69234e16f9b50b
David Neugebauer <david.neugebauer@posteo.de>
2020-01-17 14:22:29 +0100

    Refactor nlp_heideltime workers for better testability.

--- workers/nlp_heideltime/time_annotate/tasks.py
+++ workers/nlp_heideltime/time_annotate/tasks.py
@@ -1,16 +1,13 @@
 
-import logging
-
 from utils.celery_client import celery_app
 from workers.base_task import FileTask
 
-import datetime
 import os.path
-from subprocess import run, PIPE, SubprocessError
 
-from .viewer_json import convert_timeml_to_annotation_json
+from .timeml_to_viewer_json import convert_timeml_to_annotation_json
+from .time_annotate import HeideltimeCommandParamsBuilder, run_external_command
 
-log = logging.getLogger(__name__)
+
 
 
 def _determine_new_filename(input_file, target_dir, append_str):
@@ -38,73 +35,22 @@ class TimeAnnotateTask(FileTask):
     label = "Time Expression Annotation"
     description = "Annotate a textfile using the chronoi heideltime implementation."
 
-    _default_timeout_seconds = 180
-
-    _default_heideltime_params = [
-        "heideltime",
-        "-t", "narrative",
-    ]
-
-    _langs = {
-        "de": "german",
-        "en": "english",
-        "es": "spanish",
-        "fr": "french",
-        "it": "italian"
-    }
+    default_timeout_seconds = 180
 
     def process_file(self, file, target_dir):
         cmd = self._prepare_heideltime_cmd_params(file)
-        annotation_xml = self._run_external_command(cmd, self._default_timeout_seconds)
+        print(cmd)
+        annotation_xml = run_external_command(cmd, self.default_timeout_seconds)
         target_path = _determine_new_filename(file, target_dir, ".xml")
         with open(target_path, mode='wb') as out_file:
             out_file.write(annotation_xml)
 
     def _prepare_heideltime_cmd_params(self, input_file):
-        params = self._default_heideltime_params
-
-        params += ['-l', self._prepare_lang_param()]
-        params += ['-dct', self._prepare_dct_param()]
-        if self.get_param('tag_intervals'):
-            params.append('-it')
-
-        params.append(input_file)
-        return params
-
-    def _prepare_lang_param(self):
-        lang_short = self.get_param('lang')
-        if lang_short not in self._langs.keys():
-            raise ValueError(f'Language code "{lang_short}" is not a valid choice for option "lang".')
-        return self._langs[lang_short]
-
-    def _prepare_dct_param(self):
-        iso_date_string = self.get_param('document_creation_time')
-        # this should raise a ValueError if the string is not a valid date
-        date_obj = datetime.datetime.strptime(iso_date_string, "%Y-%m-%d").date()
-        if type(date_obj) is datetime.date:
-            # return the string as we are now sure that it is in valid format
-            return iso_date_string
-        else:
-            raise ValueError(f"Cannot convert date: {iso_date_string}")
-
-    def _run_external_command(self, params, timeout_after_secs):
-        log.debug("Calling: ", " ".join(params))
-        try:
-            # When calling the command, redirect stdout and stderr to separate pipes,
-            # raise exceptions in case of a non-zero return code or after the timeout passes.
-            result = run(params, stdout=PIPE, stderr=PIPE, check=True, timeout=timeout_after_secs)
-        except SubprocessError as e:
-            # Either a TimeoutExpired or a CalledProcessError.
-            # In any case we dump stderr as that is not included in the exception messages.
-            if hasattr(e, 'stderr') and e.stderr is not None:
-                log.error("Call to external program failed. Dumping stderr:")
-                log.error(e.stderr)
-            # re-raise to fail this task
-            raise e
-        if result is not None and result.stdout is not None:
-            return result.stdout
-        else:
-            return ""
-
+        builder = HeideltimeCommandParamsBuilder()
+        builder.set_language(self.get_param("lang"))
+        builder.set_dct(self.get_param("document_creation_time"))
+        builder.set_interval_tagging(self.get_param("tag_intervals"))
+        builder.set_target_filename(input_file)
+        return builder.get_params()
 
 TimeAnnotateTask = celery_app.register_task(TimeAnnotateTask())
