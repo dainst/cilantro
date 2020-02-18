1e1d357dc46db53db35e07de83aca8f789c1fb19
Nils Hempel <nhempel@dai-z-144.local>
2020-02-18 17:02:13 +0100

    new form

---
+++ frontend/src/job/AppOptionForm.vue
@@ -0,0 +1,45 @@
+<template>
+  <b-collapse
+                aria-id="processing_options_collapse"
+                class="panel"
+                :open.sync="isOpen">
+        <div
+                slot="trigger"
+                class="panel-heading"
+                role="button"
+                aria-controls="processing_options_collapse">
+            <strong>Post-Processing Options</strong>
+        </div>
+        <div class="panel-block">
+        <b-field>
+            <b-switch v-model="options.keep_staging">
+               Keep imported raw data on server
+            </b-switch>
+        </b-field>
+        </div>
+  </b-collapse>
+</template>
+
+<script lang="ts">
+ 
+import {
+    Component, Vue, Prop, Watch
+} from 'vue-property-decorator';
+import { AppOptions } from './JobParameters';
+
+@Component
+export default class AppOptionsForm extends Vue {
+    @Prop({ required: true }) initialOptions!: AppOptions;
+
+    
+    isOpen = false;
+        
+
+    options: AppOptions = this.initialOptions;
+
+    @Watch('options')
+    onOptionsChanged(options: AppOptions) {
+        this.$emit('options-updated', options);
+    }
+}
+</script>
