8015f97d6d2f2c13b06f30ea250472e5a375498f
Nils Hempel <nils.hempel@dainst.de>
2020-03-23 14:59:34 +0100

    added vuex to test

--- frontend/tests/unit/job/JobList.spec.ts
+++ frontend/tests/unit/job/JobList.spec.ts
@@ -4,8 +4,13 @@ import Vuex, {Store} from 'vuex'
 
 import JobList from '@/job/JobList.vue'
 
+const localVue = createLocalVue();
+localVue.use(Buefy)
+localVue.use(Vuex)
+
 describe("JobList.vue", () => {
-    const wrapper = shallowMount(JobList, {})
+    const store = new Store({});
+    const wrapper = shallowMount(JobList, {store, localVue})
 
     it("loads proberly", () => {
         expect(wrapper.exists()).toBe(true)
