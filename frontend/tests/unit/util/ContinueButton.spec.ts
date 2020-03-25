e6ab5937a0d0ac1e5a6d5816d6cc2823a5e4f8c2
Nils Hempel <nils.hempel@dainst.de>
2020-03-25 17:04:27 +0100

    added test for button

--- frontend/tests/unit/util/ContinueButton.spec.ts
+++ frontend/tests/unit/util/ContinueButton.spec.ts
@@ -1,5 +1,6 @@
 import {shallowMount, createLocalVue} from '@vue/test-utils';
 import Buefy from 'buefy';
+import BButton from 'buefy/src/components/button/BButton.vue';
 import Vuex, {Store} from 'vuex'
 
 import ContinueButton from '@/util/ContinueButton.vue'
@@ -8,3 +9,22 @@ const localVue = createLocalVue();
 localVue.use(Buefy)
 localVue.use(Vuex)
 
+describe("ContinueButton.vue", () => {
+    let wrapper: any;
+
+    beforeEach(()=>{
+        const store = new Store({});
+        wrapper = shallowMount(ContinueButton, {
+            localVue,
+            store,
+            methods: {
+                onClick: jest.fn()
+            }
+        });
+    })
+
+    it("renders", () => {
+        expect(wrapper.exists()).toBe(true)
+    })
+
+})
