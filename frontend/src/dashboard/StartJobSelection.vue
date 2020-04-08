7005c0199bc46c994d6a2a2288e4e8b713451c68
Simon Hohl <simon.hohl@dainst.org>
2020-04-08 12:42:26 +0200

    Rearrange job selection on dashboard

--- frontend/src/dashboard/StartJobSelection.vue
+++ frontend/src/dashboard/StartJobSelection.vue
@@ -9,7 +9,11 @@
                         Import multiple folders that contain scans of journal issues
                         into iDAI.publications / OJS.
                     </p>
-                    <router-link to="ingest-journal" tag="b-button" id="start_ingest_journal">Start</router-link>
+                    <router-link to="ingest-journal"
+                                 tag="b-button"
+                                 id="start_ingest_journal">
+                        Start
+                    </router-link>
                 </div>
                 <div class="tile is-child box">
                     <p class="title">Retrodigitized Books</p>
@@ -17,7 +21,11 @@
                         Import multiple folders that contain scans of Book issues
                         into iDAI.publications / OMP.
                     </p>
-                    <router-link to="ingest-book" tag="b-button" id="start_ingest_book">Start</router-link>
+                    <router-link to="ingest-book"
+                                 tag="b-button"
+                                 id="start_ingest_book">
+                        Start
+                    </router-link>
                 </div>
                 <div class="tile is-child box">
                     <p class="title">Retrodigitized Archival Material</p>
@@ -25,8 +33,16 @@
                         Import multiple folders that contain scans of archival material
                         into iDAI.archives / AtoM.
                     </p>
-                    <router-link to="ingest-archival-material" tag="b-button" id="start_ingest_archival_material">Start</router-link>
+                    <router-link to="ingest-archival-material"
+                                 tag="b-button"
+                                 id="start_ingest_archival_material">
+                        Start
+                    </router-link>
                 </div>
+            </div>
+        </div>
+        <div class="tile is-ancestor">
+            <div class="tile is-parent">
                 <div class="tile is-child box">
                     <p class="title">NLP Processing</p>
                     <p>
