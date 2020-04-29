d7ad6fe3884f741e205bc4386bb0d6cc8c89cd75
Simon Hohl <simon.hohl@dainst.org>
2020-04-29 13:01:32 +0200

    Apply linter fixes

--- frontend/tests/unit/Login.spec.ts
+++ frontend/tests/unit/Login.spec.ts
@@ -1,74 +1,65 @@
-import {shallowMount, mount, createLocalVue } from '@vue/test-utils';
-import { mocked } from 'ts-jest/utils';
+import { mount, createLocalVue } from '@vue/test-utils';
 import Buefy from 'buefy';
-import Vuex, {Store} from 'vuex'
+import Vuex, { Store } from 'vuex';
+import Router from 'vue-router';
 import AuthenticationStore from '@/authentication/AuthenticationStore';
 import AuthenticationStatus from '@/authentication/AuthenticationStatus';
-import Login from '@/Login.vue'
-import * as notifiers from '@/util/Notifier.ts'
-import Router from 'vue-router';
+import Login from '@/Login.vue';
 
+const MockComponent = { template: '<div class="login_success">Login success</div>' };
 
-const MockComponent = { template: '<div class="login_success">Login success</div>' }
-
-const routes = [ 
+const routes = [
     {
-            path: '/login',
-            name: 'login',
-            component: Login,
-            meta: {
-                noAuth: true
-            }
-        },{
-        path: "/",
+        path: '/login',
+        name: 'login',
+        component: Login,
+        meta: {
+            noAuth: true
+        }
+    }, {
+        path: '/',
         name: 'login_sucess',
         component: MockComponent
-    } 
+    }
 ];
 
 const mockLogin = jest.fn();
-jest.mock('@/authentication/AuthenticationStore', () => {
-    return jest.fn().mockImplementation(() => {
-        return {
-            login: mockLogin,
-        };
-    })
-});
+jest.mock('@/authentication/AuthenticationStore', () => jest.fn().mockImplementation(() => ({
+    login: mockLogin
+})));
 
 jest.mock('@/util/Notifier.ts', () => ({
     showError() {
         mockLogin();
         return true;
     }
-}))
+}));
 
-const localVue = createLocalVue()
-localVue.use(Buefy)
-localVue.use(Vuex)
-localVue.use(Router)
+const localVue = createLocalVue();
+localVue.use(Buefy);
+localVue.use(Vuex);
+localVue.use(Router);
 
-describe("Login.vue", () => {
+describe('Login.vue', () => {
     let wrapper: any;
-    let authStore = new AuthenticationStore({});
+    const authStore = new AuthenticationStore({});
     let store: any;
     let router: any;
-    beforeEach(()=>{
-      router = new Router({ routes });
-      store = new Store({
+    beforeEach(() => {
+        router = new Router({ routes });
+        store = new Store({
             state: {
                 status: AuthenticationStatus.Out
             },
             getters: {
-                authStatus: (state) => {
-                    return state.status;
-                }
+                authStatus: state => state.status
             },
             mutations: {
                 setError(state: any) {
-                    state.status = AuthenticationStatus.Error
+                    state.status = AuthenticationStatus.Error;
                 },
                 setSuccess(state: any) {
-                    state.status = AuthenticationStatus.In
+                    state.status = AuthenticationStatus.In;
                 }
             }
         });
@@ -77,44 +68,37 @@ describe("Login.vue", () => {
             store,
             router,
             methods: {
-                 createAuthStore () { return authStore }
-            },
-           
+                createAuthStore() { return authStore; }
+            }
 
-        }); 
-        
-    })
+        });
+    });
 
-    it("renders", () => {
-        expect(wrapper.exists()).toBe(true)
-    })
+    it('renders', () => {
+        expect(wrapper.exists()).toBe(true);
+    });
 
-    it("submits the login form", async () => {
-
+    it('submits the login form', async() => {
         wrapper.find('#username').setValue('santa');
         wrapper.find('#password').setValue('klaus');
         await wrapper.vm.$nextTick();
 
-        let button = wrapper.find('button');
+        const button = wrapper.find('button');
         expect(button.attributes('disabled')).toBeFalsy();
         button.trigger('click');
-        wrapper.vm.$nextTick();
+        await wrapper.vm.$nextTick();
 
         expect(mockLogin).toBeCalled();
+    });
 
-    })
-
-    it("shows an login failed error", async () => {
+    it('shows an login failed error', async() => {
         store.commit('setError');
         expect(wrapper.exists('.snackbar')).toBeTruthy();
-        
-    })
+    });
 
-    it("calls router push to /", async () => {
+    it('calls router push to /', async() => {
         store.commit('setSuccess');
         await wrapper.vm.$nextTick();
         expect(wrapper.exists('div.login_success')).toBe(true);
-      
-    })
-
-})
+    });
+});
