9fc5dc1bb8ab11f96b254ce0b516734ff2a4cbca
Simon Hohl <simon.hohl@dainst.org>
2021-07-26 09:13:50 +0200

    Reduce tasks per child to 1 for convert workers

--- docker/cilantro-convert-worker/entrypoint.sh
+++ docker/cilantro-convert-worker/entrypoint.sh
@@ -3,5 +3,5 @@ if [ "$CILANTRO_ENV" = "development" ]
 then
     watchmedo auto-restart -R -d service -d config -d workers -p="*.py;*.yml" -- celery -A workers.convert.tasks -Q convert worker --loglevel=info
 else
-    celery -A workers.convert.tasks -Q convert worker --loglevel=info --max-tasks-per-child=5
+    celery -A workers.convert.tasks -Q convert worker --loglevel=info --max-tasks-per-child=1
 fi
