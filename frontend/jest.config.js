be39fc6e23953b559176c11fd108fb2d5efdf4ce
Simon Hohl <simon.hohl@dainst.org>
2021-10-04 12:15:13 +0200

    Fix npm issues

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -4,15 +4,15 @@ module.exports = {
         '^.+\\.vue$': 'vue-jest',
         '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
         '^.+\\.tsx?$': 'ts-jest',
-        "^.+\\.js$": "<rootDir>/node_modules/babel-jest"
-          },
+        '^.+\\.js$': '<rootDir>/node_modules/babel-jest'
+    },
     transformIgnorePatterns: [
-              '<rootDir>/node_modules/(?!@babel)'
+        '<rootDir>/node_modules/(?!@babel)'
     ],
     moduleNameMapper: {
-                     '^@/(.*)$': '<rootDir>/src/$1'
+        '^@/(.*)$': '<rootDir>/src/$1'
     },
     testMatch: [
-        '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
-    ],
-}
+        '<rootDir>/(tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx))'
+    ]
+};
