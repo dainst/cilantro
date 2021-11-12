36efd951c8d19228a0d3f7e67efda7bc6a22a04b
Simon Hohl <simon.hohl@dainst.org>
2021-11-12 15:01:44 +0100

    Add basic render test: journal target data/error

--- frontend/tests/unit/job/ingest-journal/IngestJournalMetadataForm.spec.ts
+++ frontend/tests/unit/job/ingest-journal/IngestJournalMetadataForm.spec.ts
@@ -1,13 +1,13 @@
 import {
-    shallowMount, mount, createLocalVue
+    mount, createLocalVue
 } from '@vue/test-utils';
 import Buefy from 'buefy';
 import Vuex, { Store } from 'vuex';
-import flushPromises from 'flush-promises';
 import IngestJournalMetadataForm from '@/job/ingest-journal/IngestJournalMetadataForm.vue';
-import { WorkbenchFileTree } from '@/staging/StagingClient';
 
 import stagingTreeMock from '@/../tests/resources/ingest-journal/staging-tree-mock.json';
+import { JobTargetError } from '@/job/JobParameters';
+import { JobTargetData, JournalIssueMetadata } from '@/job/ingest-journal/IngestJournalParameters';
 
 jest.mock('@/staging/StagingClient', () => ({
     ...jest.requireActual('@/staging/StagingClient'),
@@ -24,7 +24,7 @@ describe('IngestJournalMetadataForm', () => {
 
     beforeEach(() => {
         store = new Store({});
-        wrapper = shallowMount(IngestJournalMetadataForm, {
+        wrapper = mount(IngestJournalMetadataForm, {
             localVue,
             store,
             propsData: {
@@ -67,5 +67,36 @@ describe('IngestJournalMetadataForm', () => {
 
         const result = wrapper.vm.evaluateTargetFolder(copy);
         expect(result).toContain("Subfolder 'tif' does not exclusively contain TIF files.");
+    });
+
+    it('invalid target causes rendered errors', async() => {
+        const error = new JobTargetError('target_id', 'target_path', ['example error']);
+
+        await wrapper.setData({ targets: [error] });
+        // Toggle detail view
+        await wrapper.find('tbody > tr > td.chevron-cell > a').trigger('click');
+
+        expect(wrapper.find('.detail-container li').text()).toEqual('example error');
+    });
+
+    it('valid targets are rendered', async() => {
+        const targets = [
+            new JobTargetData(
+                'target_id_1', 'target_path_1', new JournalIssueMetadata('000000000', 'journal_name_1', 'ojs_journal_code_1', 'issue_title_1')
+            ),
+            new JobTargetData(
+                'target_id_2', 'target_path_2', new JournalIssueMetadata('000000001', 'journal_name_2', 'ojs_journal_code_2', 'issue_title_2')
+            )
+        ];
+
+        await wrapper.setData({ targets });
+
+        expect(wrapper.findAll('tbody tr')).toHaveLength(2);
+
+        expect(wrapper.findAll('tbody tr td[data-label="Directory"]').at(0).text()).toContain('target_path_1');
+        expect(wrapper.findAll('tbody tr td[data-label="Title"]').at(0).text()).toContain('journal_name_1');
+
+        expect(wrapper.findAll('tbody tr td[data-label="Directory"]').at(1).text()).toContain('target_path_2');
+        expect(wrapper.findAll('tbody tr td[data-label="Title"]').at(1).text()).toContain('journal_name_2');
     });
 });
